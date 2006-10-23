2a6c86369247c99eda81b99dcb87b7a61c490e00
thomasabeel <thomasabeel@3162d2cb-3e1f-0410-96b0-914c58786302>
2006-10-23 16:07:13 +0000

    Defined the clustering interface
    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@10 3162d2cb-3e1f-0410-96b0-914c58786302

--- net/sf/javaml/clustering/Clusterer.java
+++ net/sf/javaml/clustering/Clusterer.java
@@ -25,6 +25,44 @@
 
 package net.sf.javaml.clustering;
 
+import java.util.Map;
+
+import net.sf.javaml.core.Dataset;
+import net.sf.javaml.core.Instance;
+
 public interface Clusterer {
+    /**
+     * Create a clusterer with the give dataset.
+     * 
+     * @param data
+     *            the dataset to be used to create the clusterer
+     */
+    public void buildClusterer(Dataset data);
+
+    /**
+     * Predict to which cluster the instance belongs.
+     * 
+     * @param instance
+     *            the instance to be clustered
+     * @return the predicted class value (cluster) of the instance
+     */
+    public Number predictCluster(Instance instance);
+
+    /**
+     * Predict the memberschip distribution of the instance for all the clusters
+     * in the clusterer.
+     * 
+     * @param instance
+     * @return a map with the class value (cluster) as key and a double denoting
+     *         the membership as value for each key.
+     */
+    public Map<Number, Double> predictMembershipDistribution(Instance instance);
+
+    /**
+     * Gives the number of clusters in the clusterer.
+     * 
+     * @return
+     */
+    public int getNumberOfClusters();
 
 }
