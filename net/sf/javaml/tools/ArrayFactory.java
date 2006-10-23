8e48853c383fa732f42f57dc9277547d57f1ca82
thomasabeel <thomasabeel@3162d2cb-3e1f-0410-96b0-914c58786302>
2006-10-23 23:28:19 +0000

    tool to create prefilled double arrays
    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@13 3162d2cb-3e1f-0410-96b0-914c58786302

---
+++ net/sf/javaml/tools/ArrayFactory.java
@@ -0,0 +1,39 @@
+/**
+ * ArrayFactory.java, 24-okt-06
+ *
+ * This file is part of the Java Machine Learning API
+ * 
+ * php-agenda is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * The Java Machine Learning API is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with the Java Machine Learning API; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ * Copyright (c) 2006, Thomas Abeel
+ * 
+ * Project: http://sourceforge.net/projects/java-ml/
+ * 
+ */
+
+package net.sf.javaml.tools;
+
+public class ArrayFactory {
+
+	public static double[] createDoubleArray(int length, double value) {
+		double[]tmp=new double[length];
+		for(int i=0;i<tmp.length;i++){
+			tmp[i]=value;
+		}
+		return tmp;	
+	}
+
+
+}
