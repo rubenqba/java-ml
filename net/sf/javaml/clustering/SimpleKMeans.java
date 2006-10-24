fa3666ed7177925bb1154b1fecfe889ba87520e9
thomasabeel <thomasabeel@3162d2cb-3e1f-0410-96b0-914c58786302>
2006-10-24 14:54:58 +0000

    Simple K-means implementation
    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@22 3162d2cb-3e1f-0410-96b0-914c58786302

--- net/sf/javaml/clustering/SimpleKMeans.java
+++ net/sf/javaml/clustering/SimpleKMeans.java
@@ -67,6 +67,11 @@ public class SimpleKMeans implements Clusterer {
     }
 
     /**
+     * The centroids of the different clusters.
+     */
+    private Instance[] centroids;
+
+    /**
      * Create a new Simple K-means clusterer with the given number of clusters
      * and iterations. The internal random generator is a new one based upon the
      * current system time. For the distance we use the Euclidean n-space
@@ -128,7 +133,7 @@ public class SimpleKMeans implements Clusterer {
         // centroids.
         Instance min = data.getMinimumInstance();
         Instance max = data.getMaximumInstance();
-        Instance[] centroids = new Instance[numberOfClusters];
+        centroids = new Instance[numberOfClusters];
         int instanceLength = data.getInstance(0).size();
         for (int j = 0; j < numberOfClusters; j++) {
             double[] randomInstance = new double[instanceLength];
@@ -171,20 +176,20 @@ public class SimpleKMeans implements Clusterer {
                     countPosition[assignment[i]]++;
                 }
             }
-            centroidsChanged=false;
-            for(int i=0;i<this.numberOfClusters;i++){
-                double []tmp=new double[instanceLength];
-                for(int j=0;j<instanceLength;j++){
-                    tmp[j]=sumPosition[i][j]/countPosition[i];
+            centroidsChanged = false;
+            for (int i = 0; i < this.numberOfClusters; i++) {
+                double[] tmp = new double[instanceLength];
+                for (int j = 0; j < instanceLength; j++) {
+                    tmp[j] = sumPosition[i][j] / countPosition[i];
                 }
-                Instance newCentroid=new SimpleInstance(tmp);
-                if(dm.calculateDistance(newCentroid,centroids[i])>0.0001){
-                    centroidsChanged=true;
-                    centroids[i]=newCentroid;
+                Instance newCentroid = new SimpleInstance(tmp);
+                if (dm.calculateDistance(newCentroid, centroids[i]) > 0.0001) {
+                    centroidsChanged = true;
+                    centroids[i] = newCentroid;
                 }
-                    
+
             }
-            
+
         }
 
     }
@@ -194,8 +199,18 @@ public class SimpleKMeans implements Clusterer {
     }
 
     public int predictCluster(Instance instance) {
-        // TODO Auto-generated method stub
-        return 0;
+        if (this.centroids == null)
+            throw new RuntimeException("The cluster should first be constructed");
+        int tmpCluster = -1;
+        double minDistance = Double.MAX_VALUE;
+        for (int i = 0; i < this.numberOfClusters; i++) {
+            double dist=dm.calculateDistance(centroids[i],instance);
+            if(dist<minDistance){
+                minDistance=dist;
+                tmpCluster=i;
+            }
+        }
+        return tmpCluster;
     }
 
     public double[] predictMembershipDistribution(Instance instance) {
