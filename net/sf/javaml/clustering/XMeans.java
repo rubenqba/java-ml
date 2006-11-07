2c45e72c21c8103fe095a11617df14f17c0ab14e
andreasderijcke <andreasderijcke@3162d2cb-3e1f-0410-96b0-914c58786302>
2006-11-07 18:13:36 +0000

    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@36 3162d2cb-3e1f-0410-96b0-914c58786302

--- net/sf/javaml/clustering/XMeans.java
+++ net/sf/javaml/clustering/XMeans.java
@@ -26,183 +26,198 @@
 
 package net.sf.javaml.clustering;
 
-import java.util.Random;
+
+import java.util.Vector;
 
 import net.sf.javaml.core.Dataset;
 import net.sf.javaml.core.Instance;
-import net.sf.javaml.core.SimpleInstance;
+import net.sf.javaml.core.SimpleDataset;
 import net.sf.javaml.core.BICScore; 
-import net.sf.javaml.distance.DistanceMeasure;
-import net.sf.javaml.distance.DistanceMeasureFactory;
 
 
-public class XMeans implements Clusterer  {
-		
-	private int minNumClusters = 2;
-	private int maxNumClusters = 4;
-	private int numberOfClusters = 2;
-	private int numberOfIterations = -1;
-	private Random rg;
-	private DistanceMeasure dm;
-	private Instance[] centroids;
-	private int[] assignment;
+public class XMeans extends SimpleKMeans implements Clusterer  {
+//	TODO delete following remark:	
+//	code not yet complete!
+	
+	// min value for k.
+	private int kMin;
+	// max value for k.
+	private int kMax;
+	// dimension of an instance: 250 bp
 	private int dataDimension = 250;
-	private double overAllBicScore = 0;
+	// vector of the different variance values, belonging to the local clusters in step2, according to a certain k.
+	private Vector<Double> localVariances = new Vector<Double>();
+	// total variance of all data, mean value of all local variance values according to a certain k.
+	private double totalVariance = 0;
+	// sum of all local loglikes, according to a certain k. 
+	private double totalLoglike = 0;
+	//total BIC score calculated over all data, according to a certain k.
+	private double totalBICScore = 0;
+	// vector, which temporary holds all total BIC scores belonging to all k values.
+	private Vector<Double> allTotalBicScores = new Vector<Double>();
+	// vector, which temporary holds all centroids belonging to a certain k.
+	private Vector<Instance> tempCentroids = new Vector<Instance>();
+	// vector, which temporary holds all tempCentroids belonging to all k values.
+	private Vector<Vector> allTempCentroids = new Vector<Vector>();
+	// vector, which holds the final centroids belonging to the final clustering result.
+	private Vector<Instance> finalCentroids = new Vector<Instance>();
+	// number of clusters in step 1., value between kMin and kMax.
+
+	// number of clusters resulting after step 2. for a certain k.
+	private int tempClusters;
+	// vector, wich holds all  tempClusters for each k value.
+	private Vector<Integer> totalTempClusters = new Vector<Integer>();
+	// number of cluster belonging the final clustering result.
+	private int resultingNumberOfClusters;
 	
-//TODO delete following remark:	
-//code not yet complete!
-	
-/** step 1. Improver parameters:
- * Apply k-means to dataset with first (minimum) value for 'numberOfClusters'
-*/
 	
 	
 	public void buildClusterer(Dataset data) {
 		if (data.size() == 0)
-			throw new RuntimeException("The dataset should not be empty");
-		if(numberOfClusters==0)
-		    throw new RuntimeException("There should be at least one cluster");
-		        // Place K points into the space represented by the objects that are
-		        // being clustered. These points represent the initial group of
-		        // centroids.
-		    Instance min = data.getMinimumInstance();
-		    Instance max = data.getMaximumInstance();
-		    this.centroids = new Instance[numberOfClusters];
-		    int instanceLength = data.getInstance(0).size();
-		    for (int j = 0; j < numberOfClusters; j++) {
-		    	double[] randomInstance = new double[instanceLength];
-		    	for (int i = 0; i < instanceLength; i++) {
-		    		double dist = Math.abs(max.getValue(i) - min.getValue(i));
-		    		randomInstance[i] = (min.getValue(i) + rg.nextDouble() * dist);
-		    	}
-		        this.centroids[j] = new SimpleInstance(randomInstance);
-		    }
-
-		    int iterationCount = 0;
-		    boolean centroidsChanged = true;
-		    while (iterationCount < this.numberOfIterations && centroidsChanged) {
-		    	iterationCount++;
-		            // Assign each object to the group that has the closest centroid.
-		        int[] assignment = new int[data.size()];
-		        for (int i = 0; i < data.size(); i++) {
-		        	int tmpCluster = -1;
-		            double minDistance = Double.MAX_VALUE;
-		            for (int j = 0; j < centroids.length; j++) {
-		            	double dist = dm.calculateDistance(centroids[j], data.getInstance(i));
-		                if (dist < minDistance) {
-		                    minDistance = dist;
-		                    tmpCluster = j;
-		                }
-		             }
-		             assignment[i] = tmpCluster;
+            throw new RuntimeException("The dataset should not be empty");
+        if(kMin==0)
+            throw new RuntimeException("There should be at least one cluster");
+        
+        for(int k = kMin; k == kMax; k++){
+        	/** step 1. Improve parameters:
+        	 * Apply k-means to initial dataset with k alternating from kMin to kMax
+        	 */
+	        //Clusterer km=new super(k,100);
+        	this.numberOfClusters=k;
+        	this.numberOfIterations=100;
+        	super.buildClusterer(data);
+        	
+	        Dataset[]datas=new Dataset[k];
+	        for(int i=0;i<k;i++){
+	            datas[i]=new SimpleDataset();
+	        }
+	        for(int i=0;i<data.size();i++){
+	            Instance in=data.getInstance(i);
+	            datas[super.predictCluster(in)].addInstance(in);
+	        }
+	        /** Step 2. Improve Structure:
+	         * Apply k-means with k = 2 to each cluster obtained in step 1 (parents)
+	         * and calculate BIC score for parent (k=1) and both child clusters.   
+	         * The situation with the highest BIC score is assumed as best.
+	         */
+	        for (int i = 0; i < k; i++){
+	        	
+	        	// 1. calculate BIC for parent cluster j.
+	        	int initialDataSetSize = data.size();
+	        	Instance centroidP = super.centroids[i];
+	        	// 1.1 calculate variance estimate.
+	        	double varianceP = BICScore.varianceEstimate(datas [i], centroidP, k, initialDataSetSize);
+	        	// 1.2 calculate loglikelihood.
+	        	double loglikeP = BICScore.logLikeliHood(datas[i], varianceP,k, initialDataSetSize, dataDimension);
+	        	// 1.3 calculate bic score.
+	        	double scoreP = BICScore.bicScore(datas[i], loglikeP, varianceP, k, dataDimension);
+	        	
+	        	// 2. apply K-Means with k = 2 on data assigned to centroid[i].
+	        	SimpleKMeans kmLocal=new SimpleKMeans(2,100);
+		        kmLocal.buildClusterer(datas[i]);
+		        Dataset[]datasLocal=new Dataset[2];
+		        for(int j = 0; j < 2; j++){
+		            datasLocal[j]=new SimpleDataset();
 		        }
-		            // When all objects have been assigned, recalculate the positions of
-		            // the K centroids and start over.
-		            // The new position of the centroid is the middle of the current
-		            // cluster.
-		         double[][] sumPosition = new double[this.numberOfClusters][instanceLength];
-		         int[] countPosition = new int[this.numberOfClusters];
-		         for (int i = 0; i < data.size(); i++) {
-		        	 for (int j = 0; j < instanceLength; j++) {
-		        		 double value=data.getInstance(i).getValue(j);
-		                 sumPosition[assignment[i]][j] += value;
-		                 countPosition[assignment[i]]++;
-		             }
-		         }
-		         centroidsChanged = false;
-		         for (int i = 0; i < this.numberOfClusters; i++) {
-		        	 if (countPosition[i] > 0) {// when there are no instances
-		                                            // associated with this centroid, it
-		                                            // remains the same.
-		        		 double[] tmp = new double[instanceLength];
-		                 for (int j = 0; j < instanceLength; j++) {
-		                	 tmp[j] = sumPosition[i][j] / countPosition[i];
-		                 }
-		                 Instance newCentroid = new SimpleInstance(tmp);
-		                 if (dm.calculateDistance(newCentroid, centroids[i]) > 0.0001) {
-		                        centroidsChanged = true;
-		                        centroids[i] = newCentroid;
-		                 }
-		        	 }
-		         }
-		    }
+		        for(int j = 0; j < datas[i].size(); j++){
+		            Instance in=data.getInstance(j);
+		            datasLocal[kmLocal.predictCluster(in)].addInstance(in);
+		        }
+		        // 3. calculate BIC for child clusters.
+		        int parentDataSetSize = datas[i].size();
+		        Instance centroidC1 = kmLocal.centroids[0];
+		        Instance centroidC2 = kmLocal.centroids[1];
+		        // 3.1 calculate variance estimate for both child clusters.
+		        double variance1 = BICScore.varianceEstimate(datasLocal[0], centroidC1, 2, parentDataSetSize);
+		        double variance2 = BICScore.varianceEstimate(datasLocal[1], centroidC2, 2, parentDataSetSize);	
+		        // 3.2 calculate loglikelihood for both child clusters.
+		        double loglike1 = BICScore.logLikeliHood (datasLocal[0], variance1, 2, parentDataSetSize, dataDimension);
+		        double loglike2 = BICScore.logLikeliHood (datasLocal[1], variance2, 2, parentDataSetSize, dataDimension);
+		        // 3.3 calculate over-all loglike for child clusters = sum both loglikes.
+		        double loglikeC = loglike1 + loglike2;
+		        // 3.4 calculate over-all variance for both child clusters = mean both variances.
+		        double varianceC = variance1 + variance2;
+		        // 3.4 calculate over-all bic score for child clusters.
+		        double scoreC = BICScore.bicScore(datas[i], loglikeC, varianceC, k, dataDimension);
+		        
+		        // 4. save model with highest BIC score as final model.
+		        if (scoreP>scoreC){
+		        	// add loglike to overAllLoglike.
+		        	totalLoglike += loglikeP;
+		        	// save variance. 
+		        	localVariances.add(varianceP);
+		        	// save parent centroid as partial result for current k. 
+		        	tempCentroids.add(centroidP);
+		        	tempClusters++;
+		        }
+		        else{
+		        	//add loglike to overAllLoglike. 
+		        	totalLoglike += loglikeC;
+		        	// save variance.
+		        	localVariances.add(varianceC);
+		        	// keep child centroids as partial result for current k.
+		        	tempCentroids.add(centroidC1);
+		        	tempCentroids.add(centroidC2);
+		        	tempClusters += 2;
+		        }
+		        
+			}
+	        // save final number of clusters for current k.
+	        totalTempClusters.add(tempClusters);
+	        // save final resulting centroids for current k. 
+	        allTempCentroids.add(tempCentroids);
+	        // calculate over-all variance for the whole data set as mean of sub-variances for current k.
+	        int lvs  = localVariances.size();
+	        for(int i = 0; i < lvs; i++){
+	        	totalVariance += localVariances.get(i);
+	        }
+	        totalVariance /= lvs;
+	        // calculate over-all BIC score for current k add save.
+	        totalBICScore = BICScore.bicScore(data, totalLoglike, totalVariance, k, dataDimension);
+	        allTotalBicScores.add(totalBICScore);
+	        
+		}
+        // for k with kMin < k < kMax:
+        // find highest overallBICScore.
+        int oabss  = allTotalBicScores.size();
+        int index = 0;
+        for (int i = 1; i < oabss; i++){
+        	double largest = allTotalBicScores.get(0); 
+        	if (allTotalBicScores.get(i)> largest){
+        		largest = (allTotalBicScores.get(i));
+        		index = i;
+        	}
+        }
+        // find the belonging/ appropriate centroids.
+        finalCentroids = allTempCentroids.get(index);
+        resultingNumberOfClusters = totalTempClusters.get(index);
+        
 	}
 
 	public int getNumberOfClusters() {
-		return this.numberOfClusters;
+		return this.resultingNumberOfClusters;
 	}
 
+	
 	public int predictCluster(Instance instance) {
-		//TODO implement
-		return 0;
+		if (this.finalCentroids == null)
+            throw new RuntimeException("The cluster should first be constructed");
+        int tmpCluster = -1;
+        double minDistance = Double.MAX_VALUE;
+        for (int i = 0; i < this.resultingNumberOfClusters; i++) {
+            double dist = dm.calculateDistance(finalCentroids.get(i), instance);
+            if (dist < minDistance) {
+                minDistance = dist;
+                tmpCluster = i;
+            }
+        }
+        return tmpCluster;
 	}
 
+	
 	public double[] predictMembershipDistribution(Instance instance) {
-		//TODO implement
-		return null;
-	}
-
-/** Step 2. Improve Structure:
- * Apply k-means with K = 2 to each cluster (parents)obtained in step 1.
- * Calculate BIC score for parent (K=1) and child (K = 2) clusters.   
- * The situation with the highest BIC score is assumed as best.
- */
-		
-	public void localKMeans(Dataset data) {
-		
-		for (int j=0; j < numberOfClusters; j++){
-			
-			// calculate BIC for parent cluster
-			Instance centroidp = centroids[j];
-			int number = j;
-			//TODO get assignment values from part1.
-			int assignment[]; 
-			// calculate variance estimate
-			double variance = varianceEstimate(data, centroidp, number, assignment [], numberOfClusters);
-			// calculate cluster size
-			int clusterSizeP = localClusterSize (assignment[],number);	
-			//calculate loglikelihood
-			double loglikeP = logLikeliHood (data, variance,numberOfClusters, clusterSizeP, dataDimension);
-			//calculate bic score
-			double scoreP = bicScore (data, loglikeP, variance, numberOfClusters, dataDimension);
-
-			
-			// TODO implement K-Means on data assigned to centroid[i]with k = 2 (local K-means).
-			
-			
-			//calculate BIC for child clusters
-			// TODO rename some variables
-			//TODO get assignment values from local K-means
-			int assignment[];
-//			calculate variance estimate for both child clusters (CC)
-			double variance1 = varianceEstimate(data, centroids[0], 0, assignment [], 2);
-			double variance2 = varianceEstimate(data, centroids[1], 1, assignment [], 2);
-			// calculate cluster sizes for both child clusters
-			int clusterSize1 = localClusterSize (assignment[], 0);
-			int clusterSize2 = localClusterSize (assignment[], 1);		
-			//calculate loglikelihood for both child clusters
-			double loglike1 = logLikeliHood (data, variance1,numberOfClusters, clusterSize1, dataDimension);
-			double loglike2 = logLikeliHood (data, variance2,numberOfClusters, clusterSize2, dataDimension);
-			// calculate over-all loglike for child clusters = sum both loglikes
-			double loglikeC = loglike1 + loglike2;
-			//calculate over-all bic score for child clusters 
-			double scoreC = bicScore (data, loglikeC, variance, numberOfClusters, dataDimension);
-		
-			
-			if (scoreP>scoreC){
-			// keep parent cluster as final result
-//				TODO implement	
-			}
-			else{
-			// keep child clusters as final result
-//				TODO implement
-			}
-			}
-		}
-		
-		
-	}
-		 
-	  
-	      
-		
+        double[] tmp = new double[this.getNumberOfClusters()];
+        tmp[this.predictCluster(instance)] = 1;
+        return tmp;
+    }
+}
