08beff9ea693c761b2f68ad9c22ad42d325da319
andreasderijcke <andreasderijcke@3162d2cb-3e1f-0410-96b0-914c58786302>
2006-11-16 18:05:26 +0000

    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@81 3162d2cb-3e1f-0410-96b0-914c58786302

---
+++ net/sf/javaml/clustering/evaluation/MinMaxCut.java
@@ -0,0 +1,85 @@
+/**
+ * MinMaxCut.java, 16-nov-2006
+ *
+ * This file is part of the Java Machine Learning API
+ * 
+ * php-agenda is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * The Java Machine Learning API is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with the Java Machine Learning API; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ * Copyright (c) 2006, Thomas Abeel, Andreas De Rijcke
+ * 
+ * Project: http://sourceforge.net/projects/java-ml/
+ * 
+ */
+package net.sf.javaml.clustering.evaluation;
+
+import net.sf.javaml.clustering.Clusterer;
+import net.sf.javaml.core.Dataset;
+import net.sf.javaml.core.Instance;
+import net.sf.javaml.core.SimpleDataset;
+import net.sf.javaml.distance.DistanceMeasure;
+import net.sf.javaml.distance.DistanceMeasureFactory;
+
+/**
+ * G_1 from the Zhao 2001 paper
+ * 
+ * TODO uitleg
+ * @author Andreas De Rijcke
+ *
+ */
+
+public class MinMaxCut implements ClusterEvaluation{
+	
+	public double score (Clusterer c, Dataset data){
+		Dataset[] datas = new Dataset[c.getNumberOfClusters()];
+        for (int i = 0; i < c.getNumberOfClusters(); i++) {
+            datas[i] = new SimpleDataset();
+        }
+        for (int i = 0; i < data.size(); i++) {
+            Instance in = data.getInstance(i);
+            datas[c.predictCluster(in)].addInstance(in);
+        }
+
+        DistanceMeasure dm=DistanceMeasureFactory.getCosineSimilarity();
+        double sum=0;
+        for(int i=0;i<c.getNumberOfClusters();i++){
+        	double tmpTop=0;
+        	for(int j=0;j<datas[i].size();j++){
+                for(int k=0;k<data.size();k++){
+                    // TODO
+                	// voorwaarde niet correct
+                	if ( j != k){
+                    	double error=dm.calculateDistance(datas[i].getInstance(j),data.getInstance(k));
+                        tmpTop += error;  
+                    }
+                }  
+            }
+            double tmp=0;
+                for(int j=0;j<datas[i].size();j++){
+                    for(int k=0;k<datas[i].size();k++){
+                        double error=dm.calculateDistance(datas[i].getInstance(j),datas[i].getInstance(k));
+                        tmp += error; 
+                    }   
+                }
+          double tmpSum = tmpTop/tmp;
+          sum += tmpSum;
+        }
+	return sum;
+	}
+	
+	public boolean compareScore(double score1, double score2) {
+        //should be minimized
+        return score2<score1;
+	}
+}
