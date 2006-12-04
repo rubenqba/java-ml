ae5ffc8ded5a8ce651f30631c949c2d5dd01e2f1
thomasabeel <thomasabeel@3162d2cb-3e1f-0410-96b0-914c58786302>
2006-12-04 16:10:23 +0000

    added a sigmoid kernel
    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@129 3162d2cb-3e1f-0410-96b0-914c58786302

---
+++ net/sf/javaml/distance/SigmoidKernel.java
@@ -0,0 +1,45 @@
+/**
+ * SigmoidKernel.java, 4-dec-2006
+ *
+ * This file is part of the Java Machine Learning API
+ * 
+ * The Java Machine Learning API is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * The Java Machine Learning API is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with the Java Machine Learning API; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ * Copyright (c) 2006, Thomas Abeel
+ * 
+ * Project: http://sourceforge.net/projects/java-ml/
+ * 
+ */
+package net.sf.javaml.distance;
+
+import net.sf.javaml.core.Instance;
+
+public class SigmoidKernel implements DistanceMeasure {
+
+    private float variance=1;
+    private NormDistance nd=new EuclideanDistance();
+    public SigmoidKernel(){
+        this(1);
+    }
+    public SigmoidKernel(float variance){
+        this.variance=variance;
+    }
+    public double calculateDistance(Instance x, Instance y) {
+       double norm=nd.calculateDistance(x,y);
+       return Math.pow(Math.E,-(norm/this.variance));
+     
+    }
+
+}
