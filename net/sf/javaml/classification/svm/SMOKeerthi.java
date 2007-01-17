4ee45327400a3aa652b04eaddd643e91882e8b46
thomasabeel <thomasabeel@3162d2cb-3e1f-0410-96b0-914c58786302>
2007-01-17 09:00:01 +0000

    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@158 3162d2cb-3e1f-0410-96b0-914c58786302

--- net/sf/javaml/classification/svm/SMOKeerthi.java
+++ net/sf/javaml/classification/svm/SMOKeerthi.java
@@ -1,14 +1,26 @@
-/*
- * SMOKeerthi.java 
- * -----------------------
- * Copyright (C) 2005-2006  Thomas Abeel
+/**
+ * SMOKeerthi.java, 17-jan-07
+ *
+ * This file is part of the Java Machine Learning API
  * 
- * This program is distributed in the hope that it will be useful,
+ * The Java Machine Learning API is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * The Java Machine Learning API is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  * 
- * Author: Thomas Abeel
- * Created on: 17-jan-2007 - 8:34:20
+ * You should have received a copy of the GNU General Public License
+ * along with the Java Machine Learning API; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ * Copyright (c) 2006-2007, Thomas Abeel
+ * 
+ * Project: http://sourceforge.net/projects/java-ml/
+ * 
  */
 package net.sf.javaml.classification.svm;
 
