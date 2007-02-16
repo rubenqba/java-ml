15aba256ba50ab163c6901ad5864e846ec099531
thomasabeel <thomasabeel@3162d2cb-3e1f-0410-96b0-914c58786302>
2007-02-16 15:50:12 +0000

    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@171 3162d2cb-3e1f-0410-96b0-914c58786302

--- net/sf/javaml/clustering/Clusterer.java
+++ net/sf/javaml/clustering/Clusterer.java
@@ -1,5 +1,5 @@
 /**
- * Clusterer.java, 11-okt-06
+ * Clusterer.java
  *
  * This file is part of the Java Machine Learning API
  * 
@@ -17,7 +17,7 @@
  * along with the Java Machine Learning API; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  * 
- * Copyright (c) 2006, Thomas Abeel
+ * Copyright (c) 2006-2007, Thomas Abeel
  * 
  * Project: http://sourceforge.net/projects/java-ml/
  * 
@@ -26,40 +26,22 @@
 package net.sf.javaml.clustering;
 
 import net.sf.javaml.core.Dataset;
-import net.sf.javaml.core.Instance;
 
+/**
+ * A common interface for all clustering techniques. There is only one method
+ * that should be implemented.
+ * 
+ * @author Thomas Abeel
+ * 
+ */
 public interface Clusterer {
     /**
-     * Create a clusterer with the give dataset.
+     * Will execute a particular clustering on a dataset.
      * 
      * @param data
-     *            the dataset to be used to create the clusterer
+     *            the dataset on which to execute the clustering.
+     * @return the different clusters obtained by this clustering algorithm
      */
-    public void buildClusterer(Dataset data);
-
-    /**
-     * Predict to which cluster the instance belongs.
-     * 
-     * @param instance
-     *            the instance to be clustered
-     * @return the index of the cluster to which the instance belongs
-     */
-    public int predictCluster(Instance instance);
-
-    /**
-     * Predict the memberschip distribution of the instance for all the clusters
-     * in the clusterer.
-     * 
-     * @param instance
-     * @return the distribution for the membership for all clusters
-     */
-    public double[] predictMembershipDistribution(Instance instance);
-
-    /**
-     * Gives the number of clusters in the clusterer.
-     * 
-     * @return
-     */
-    public int getNumberOfClusters();
+    public Dataset[] executeClustering(Dataset data);
 
 }
