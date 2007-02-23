e1f1bc8ce7d752e6ba7c73defd63af80a3f06e82
thomasabeel <thomasabeel@3162d2cb-3e1f-0410-96b0-914c58786302>
2007-02-23 14:54:32 +0000

    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@187 3162d2cb-3e1f-0410-96b0-914c58786302

--- net/sf/javaml/clustering/evaluation/Gamma.java
+++ net/sf/javaml/clustering/evaluation/Gamma.java
@@ -17,7 +17,8 @@
  * along with the Java Machine Learning API; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  * 
- * Copyright (c) 2006, Andreas De Rijcke
+ * Copyright (c) 2006-2007, Andreas De Rijcke
+ * Copyright (c) 2006-2007, Thomas Abeel
  * 
  * Project: http://sourceforge.net/projects/java-ml/
  * 
@@ -25,10 +26,8 @@
 
 package net.sf.javaml.clustering.evaluation;
 
-import net.sf.javaml.clustering.Clusterer;
 import net.sf.javaml.core.Dataset;
 import net.sf.javaml.core.Instance;
-import net.sf.javaml.core.SimpleDataset;
 import net.sf.javaml.distance.DistanceMeasure;
 import net.sf.javaml.distance.EuclideanDistance;
 
@@ -36,26 +35,19 @@ import net.sf.javaml.distance.EuclideanDistance;
  * TODO uitleg
  * 
  * @author Andreas De Rijcke
+ * @author Thomas Abeel
  * 
  */
 public class Gamma implements ClusterEvaluation {
 
 	private DistanceMeasure dm = new EuclideanDistance();
 
-	public double score(Clusterer c, Dataset data) {
-		Dataset[] datas = new Dataset[c.getNumberOfClusters()];
-		double maxIntraDist[] = new double[c.getNumberOfClusters()];
+	public double score(Dataset[] datas) {
+		double maxIntraDist[] = new double[datas.length];
 		double sPlus = 0,sMin = 0;
-		// get clusters
-		for (int i = 0; i < c.getNumberOfClusters(); i++) {
-			datas[i] = new SimpleDataset();
-		}
-		for (int i = 0; i < data.size(); i++) {
-			Instance in = data.getInstance(i);
-			datas[c.predictCluster(in)].addInstance(in);
-		}
+		
 		// calculate max intra cluster distance
-		for (int i = 0; i < c.getNumberOfClusters(); i++) {
+		for (int i = 0; i < datas.length; i++) {
 			maxIntraDist[i] = Double.MIN_VALUE;
 			for (int j = 0; j < datas[i].size(); j++) {
 				Instance x = datas[i].getInstance(j);
@@ -70,10 +62,10 @@ public class Gamma implements ClusterEvaluation {
 		}
 		// search for min inter cluster distance
 		// count sPlus and sMin
-		for (int i = 0; i < c.getNumberOfClusters(); i++) {
+		for (int i = 0; i < datas.length; i++) {
 			for (int j = 0; j < datas[i].size(); j++) {
 				Instance x = datas[i].getInstance(j);
-				for (int k = i + 1; k < c.getNumberOfClusters(); k++) {
+				for (int k = i + 1; k < datas.length; k++) {
 					for (int l = 0; l < datas[k].size(); l++) {
 						Instance y = datas[k].getInstance(l);
 						double distance = dm.calculateDistance(x, y);
