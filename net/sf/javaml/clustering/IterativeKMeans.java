aca2d9ff4ded39ccfe69ff90a91d894a672c70dc
thomasabeel <thomasabeel@3162d2cb-3e1f-0410-96b0-914c58786302>
2007-02-23 15:56:46 +0000

    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@203 3162d2cb-3e1f-0410-96b0-914c58786302

--- net/sf/javaml/clustering/IterativeKMeans.java
+++ net/sf/javaml/clustering/IterativeKMeans.java
@@ -1,5 +1,5 @@
 /**
- * IterativeKMeans.java, 10-nov-2006
+ * IterativeKMeans.java
  *
  * This file is part of the Java Machine Learning API
  * 
@@ -17,99 +17,58 @@
  * along with the Java Machine Learning API; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  * 
- * Copyright (c) 2006, Thomas Abeel, Andreas De Rijcke
+ * Copyright (c) 2006-2007, Thomas Abeel
+ * Copyright (c) 2006-2007, Andreas De Rijcke
  * 
  * Project: http://sourceforge.net/projects/java-ml/
  * 
  */
 package net.sf.javaml.clustering;
 
-import java.util.Vector;
-
 import net.sf.javaml.clustering.evaluation.ClusterEvaluation;
-import net.sf.javaml.clustering.evaluation.ClusterEvaluationFactory;
 import net.sf.javaml.core.Dataset;
-import net.sf.javaml.core.Instance;
+import net.sf.javaml.distance.DistanceMeasure;
 
 /**
  * TODO: uitleg
  * 
- * @author Thomas Abeel, Andreas De Rijcke
+ * @author Thomas Abeel
+ * @author Andreas De Rijcke
  * 
  */
 
-public class IterativeKMeans extends SimpleKMeans {
+public class IterativeKMeans implements Clusterer {
     private int kMin;
 
     private int kMax;
 
-    public IterativeKMeans(int kMin, int kMax) {
+    private ClusterEvaluation ce;
+
+    private DistanceMeasure dm;
+
+    private int iterations;
+
+    public IterativeKMeans(int kMin, int kMax, int iterations, DistanceMeasure dm, ClusterEvaluation ce) {
         this.kMax = kMax;
+        this.iterations = iterations;
         this.kMin = kMin;
+        this.dm = dm;
+        this.ce = ce;
     }
 
-    @Override
-    public void buildClusterer(Dataset data) {
-        System.out.println("Build Iterative clusterer");
-        if (data.size() == 0)
-            throw new RuntimeException("The dataset should not be empty");
-        if (kMin == 0)
-            throw new RuntimeException("There should be at least one cluster");
-
-        int bestNumberOfClusters = 0;
-        double bestScore = 0;
-        Instance[] bestCentroids = null;
-
-        for (int k = kMin; k <= kMax; k++) {
-            super.numberOfClusters = k;
-            super.numberOfIterations = 100;
-            super.buildClusterer(data);
-
-            ClusterEvaluation ce = ClusterEvaluationFactory.getTau(); // getSumOfSquaredErrors();//getSumOfAveragePairWiseSimilarities();//getSumOfCentroidSimilarities();//ClusterEvaluationFactory.getSumOfSquaredErrors();
-            double newScore = ce.score(this, data);
-            if (k == kMin) {// for the first value, copy results
-                bestScore = newScore;
-                bestCentroids = super.centroids;
-                bestNumberOfClusters = k;
-            }
-            if (ce.compareScore(bestScore, newScore)) {// if new results are
-                                                        // better, copy them
-                                                        // over the old ones
-                bestScore = newScore;
-                bestCentroids = super.centroids;
-                bestNumberOfClusters = k;
-            }
-            System.out.println("k = " + k);
-            System.out.println("score = " + newScore);
-
-            System.out.println("Best centroids: " + bestCentroids);
-            System.out.println("new bestScore  = " + bestScore);
-            System.out.println("bestNumberOfClusters = " + bestNumberOfClusters);
-            System.out.println();
-        }
-
-        // copy centroids
-        System.out.println("Centroids: " + bestCentroids);
-        super.centroids = bestCentroids;
-        super.numberOfClusters = bestNumberOfClusters;
-        // FILTER BESTCENTROIDS
-        int[] freqTable = new int[bestNumberOfClusters];
-        for (int i = 0; i < data.size(); i++) {
-            freqTable[predictCluster(data.getInstance(i))]++;
-        }
-        Vector<Instance> tmpCentroids = new Vector<Instance>();
-        int nonEmptyClusterCount = 0;
-        for (int i = 0; i < freqTable.length; i++) {
-            if (freqTable[i] > 0) {
-                tmpCentroids.add(bestCentroids[i]);
-                nonEmptyClusterCount++;
+    public Dataset[] executeClustering(Dataset data) {
+        SimpleKMeans km = new SimpleKMeans(this.kMin, this.iterations, this.dm);
+        Dataset[] bestClusters = km.executeClustering(data);
+        double bestScore = this.ce.score(bestClusters);
+        for (int i = kMin + 1; i <= kMax; i++) {
+            km = new SimpleKMeans(i, this.iterations, this.dm);
+            Dataset[] tmpClusters = km.executeClustering(data);
+            double tmpScore = this.ce.score(tmpClusters);
+            if (this.ce.compareScore(bestScore, tmpScore)) {
+                bestScore = tmpScore;
+                bestClusters = tmpClusters;
             }
         }
-        super.centroids = new Instance[tmpCentroids.size()];
-        super.centroids = tmpCentroids.toArray(super.centroids);
-        super.numberOfClusters = nonEmptyClusterCount;
-        // System.out.println("Final centroid count: "+super.centroids.length);
-        // System.out.println("Final number of Clusters:
-        // "+super.numberOfClusters);
+        return bestClusters;
     }
 }
