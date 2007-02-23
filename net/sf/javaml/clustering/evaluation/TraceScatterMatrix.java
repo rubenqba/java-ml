9921e1300482e3728820687486814a9e5ec591d9
thomasabeel <thomasabeel@3162d2cb-3e1f-0410-96b0-914c58786302>
2007-02-23 15:07:00 +0000

    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@192 3162d2cb-3e1f-0410-96b0-914c58786302

--- net/sf/javaml/clustering/evaluation/TraceScatterMatrix.java
+++ net/sf/javaml/clustering/evaluation/TraceScatterMatrix.java
@@ -1,5 +1,5 @@
 /**
- * TraceScatterMatrix.java, 16-nov-2006
+ * TraceScatterMatrix.java
  *
  * This file is part of the Java Machine Learning API
  * 
@@ -17,89 +17,92 @@
  * along with the Java Machine Learning API; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  * 
- * Copyright (c) 2006, Andreas De Rijcke
+ * Copyright (c) 2006-2007, Andreas De Rijcke
+ * Copyright (c) 2006-2007, Thomas Abeel
  * 
  * Project: http://sourceforge.net/projects/java-ml/
  * 
  */
 package net.sf.javaml.clustering.evaluation;
 
-import net.sf.javaml.clustering.Clusterer;
 import net.sf.javaml.core.Dataset;
 import net.sf.javaml.core.Instance;
-import net.sf.javaml.core.SimpleDataset;
 import net.sf.javaml.core.SimpleInstance;
+import net.sf.javaml.distance.CosineSimilarity;
 import net.sf.javaml.distance.DistanceMeasure;
-import net.sf.javaml.distance.DistanceMeasureFactory;
 
 /**
  * E_1 from the Zhao 2001 paper
  * 
  * TODO uitleg
+ * 
  * @author Andreas De Rijcke
- *
+ * @author Thomas Abeel
  */
 
-public class TraceScatterMatrix implements ClusterEvaluation{
-		
-		public double score (Clusterer c, Dataset data) {
-			Dataset[] datas = new Dataset[c.getNumberOfClusters()];
-	        for (int i = 0; i < c.getNumberOfClusters(); i++) {
-	            datas[i] = new SimpleDataset();
-	        }
-	        for (int i = 0; i < data.size(); i++) {
-	            Instance in = data.getInstance(i);
-	            datas[c.predictCluster(in)].addInstance(in);
-	        }
-	        // calculate centroid complete dataset
-	        int instanceLength = data.getInstance(0).size();
-	        float [] sumVector = new float[instanceLength];
-	        for (int i=0; i < data.size(); i++) {
-	        	Instance in = data.getInstance(i);
-	        	for (int j = 0; j < instanceLength; j++) {
-	        		sumVector[j] += in.getValue(j)*in.getWeight();
-	        	}	
-	        }
-	        for (int j = 0; j < instanceLength; j++) {
-        		sumVector[j] /= data.size();
-        	}	
-	        Instance mCentroid = new SimpleInstance(sumVector);
-	        System.out.println(mCentroid);
-	        // calculate centroids
-	        double[][] sumPosition = new double[c.getNumberOfClusters()][instanceLength];
-	        int[] countPosition = new int[c.getNumberOfClusters()];
-	        for (int i = 0; i < data.size(); i++) {
-	            Instance in = data.getInstance(i);
-	            int predictedIndex = c.predictCluster(in);
-	            for (int j = 0; j < instanceLength; j++) {
+public class TraceScatterMatrix implements ClusterEvaluation {
+    public TraceScatterMatrix(DistanceMeasure dm) {
+        this.dm = dm;
+    }
 
-	                sumPosition[predictedIndex][j] += in.getWeight() * in.getValue(j);
+    private DistanceMeasure dm = new CosineSimilarity();
 
-	            }
-	            countPosition[predictedIndex]++;
-	        }
-	        //DistanceMeasure
-	        Instance[] centroids = new Instance[c.getNumberOfClusters()];
-	        for (int i = 0; i < c.getNumberOfClusters(); i++) {
-	            float[] tmp = new float[instanceLength];
-	            for (int j = 0; j < instanceLength; j++) {
-	                tmp[j] = (float) sumPosition[i][j] / countPosition[i];
-	            }
-	            centroids[i] = new SimpleInstance(tmp);
+    public double score(Dataset[] datas) {
 
-	        }
-	        // calculate trace of the between-cluster scatter matrix.
-	        DistanceMeasure dm = DistanceMeasureFactory.getCosineSimilarity();
-	        double sum = 0;
-	        for (int i = 0; i < c.getNumberOfClusters(); i++) {
-	                double error = dm.calculateDistance(centroids[i], mCentroid);
-	                sum += error*datas[i].size();
-	        }
-	        return sum;   
-	    }
+        // calculate centroid complete dataset
+        int instanceLength = datas[0].getInstance(0).size();
+        float[] sumVector = new float[instanceLength];
+        int count = 0;
+        for (int i = 0; i < datas.length; i++) {
+            for (int j = 0; j < datas[i].size(); j++) {
+                Instance in = datas[i].getInstance(j);
+                for (int k = 0; k < instanceLength; k++) {
+                    sumVector[k] += in.getValue(k) * in.getWeight();
+                    count++;
+                }
+            }
+        }
+        for (int j = 0; j < instanceLength; j++) {
+            sumVector[j] /= count;
+        }
+        Instance mCentroid = new SimpleInstance(sumVector);
+        System.out.println(mCentroid);
+        // calculate centroids
+        double[][] sumPosition = new double[datas.length][instanceLength];
+        int[] countPosition = new int[datas.length];
+        for (int i = 0; i < datas.length; i++) {
+            for (int j = 0; j < datas[i].size(); j++) {
+                Instance in = datas[i].getInstance(i);
 
-	    public boolean compareScore(double score1, double score2) {
-	        //should be minimized
-	        return score2<score1;
-		}
+                for (int k = 0; k < instanceLength; k++) {
+
+                    sumPosition[i][k] += in.getWeight() * in.getValue(j);
+
+                }
+                countPosition[i]++;
+            }
+        }
+        // DistanceMeasure
+        Instance[] centroids = new Instance[datas.length];
+        for (int i = 0; i < datas.length; i++) {
+            float[] tmp = new float[instanceLength];
+            for (int j = 0; j < instanceLength; j++) {
+                tmp[j] = (float) sumPosition[i][j] / countPosition[i];
+            }
+            centroids[i] = new SimpleInstance(tmp);
+
+        }
+        // calculate trace of the between-cluster scatter matrix.
+        double sum = 0;
+        for (int i = 0; i < datas.length; i++) {
+            double error = dm.calculateDistance(centroids[i], mCentroid);
+            sum += error * datas[i].size();
+        }
+        return sum;
+    }
+
+    public boolean compareScore(double score1, double score2) {
+        // should be minimized
+        return score2 < score1;
+    }
 }
