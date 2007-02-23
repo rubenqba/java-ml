cebb67af38a3b979f88e22aea4b08db840afe5fe
thomasabeel <thomasabeel@3162d2cb-3e1f-0410-96b0-914c58786302>
2007-02-23 16:23:29 +0000

    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@206 3162d2cb-3e1f-0410-96b0-914c58786302

--- net/sf/javaml/test/VisualTestIterativeKMeans.java
+++ net/sf/javaml/test/VisualTestIterativeKMeans.java
@@ -1,4 +1,4 @@
-/** VisualTestMultiKMeans.java, 25-okt-2006
+/** VisualTestMultiKMeans.java
  *
  * This file is part of the Java Machine Learning API
  * 
@@ -16,7 +16,8 @@
  * along with the Java Machine Learning API; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  * 
- * Copyright (c) 2006, Thomas Abeel, Andreas De Rijcke
+ * Copyright (c) 2006-2007, Thomas Abeel
+ * Copyright (c) 2006-2007, Andreas De Rijcke
  * 
  * Project: http://sourceforge.net/projects/java-ml/
  * 
@@ -34,10 +35,11 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import net.sf.javaml.clustering.IterativeKMeans;
-import net.sf.javaml.clustering.SimpleKMeans;
+import net.sf.javaml.clustering.evaluation.*;
 import net.sf.javaml.core.Dataset;
 import net.sf.javaml.core.Instance;
-import net.sf.javaml.core.SimpleDataset;
+import net.sf.javaml.distance.DistanceMeasure;
+import net.sf.javaml.distance.EuclideanDistance;
 import net.sf.javaml.tools.DatasetGenerator;
 
 public class VisualTestIterativeKMeans extends JPanel {
@@ -59,28 +61,24 @@ public class VisualTestIterativeKMeans extends JPanel {
     public VisualTestIterativeKMeans() {
         this.setLayout(new GridLayout(0, 5));
         int space = 200;
-        Dataset data = DatasetGenerator.createClusterSquareDataset(space, 8);
+        Dataset data = DatasetGenerator.createClusterSquareDataset(200, 8,200);
 
         this.add(createLabel(data, Color.BLACK, space, space, null, null, 0));
-        IterativeKMeans km = new IterativeKMeans(1, 10);
-        km.buildClusterer(data);
+        DistanceMeasure dm=new EuclideanDistance();
+        //IterativeKMeans km = new IterativeKMeans(2, 20,100,dm,new Tau(dm));
+        IterativeKMeans km = new IterativeKMeans(2, 20,100,dm,new SumOfSquaredErrors(dm));
+        
 
-        Dataset[] datas = new Dataset[10];
-        for (int i = 0; i < 10; i++) {
-            datas[i] = new SimpleDataset();
-        }
-        for (int i = 0; i < data.size(); i++) {
-            Instance in = data.getInstance(i);
-            datas[km.predictCluster(in)].addInstance(in);
-        }
+        Dataset[] datas=km.executeClustering(data);
+        
         Color[] colors = { Color.RED, Color.GREEN, Color.BLUE, Color.MAGENTA,Color.CYAN };
-        for (int i = 0; i < 10; i++) {
-            this.add(createLabel(datas[i], colors[i/2], space, space, km, colors, i));
+        for (int i = 0; i < datas.length; i++) {
+            this.add(createLabel(datas[i], colors[i%5], space, space, km, colors, i));
         }
 
     }
 
-    private JLabel createLabel(Dataset data, Color color, int width, int height, SimpleKMeans km, Color[] colors, int i) {
+    private JLabel createLabel(Dataset data, Color color, int width, int height, IterativeKMeans km, Color[] colors, int i) {
         return new ClusterLabel(data, color, width, height, km, colors, i);
     }
 
@@ -96,15 +94,15 @@ public class VisualTestIterativeKMeans extends JPanel {
 
         private Color color;
 
-        private SimpleKMeans km;
+        private IterativeKMeans km;
 
        private int tmpI;
 
-        public ClusterLabel(Dataset data, Color color, int width, int height, SimpleKMeans km, Color[] colors, int i) {
+        public ClusterLabel(Dataset data, Color color, int width, int height, IterativeKMeans km2, Color[] colors, int i) {
             this.setPreferredSize(new Dimension(width, height));
             this.data = data;
             this.color = color;
-            this.km = km;
+            this.km = km2;
              this.tmpI = i;
             this.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         }
@@ -118,21 +116,21 @@ public class VisualTestIterativeKMeans extends JPanel {
                 g.fillOval((int) in.getValue(0) - 1, (int) in.getValue(1) - 1, 2, 2);
 
             }
-            if (km != null) {
-                Instance[] centroids = km.getCentroids();
-                for (int i = 0; i < centroids.length; i++) {
-                    g.setColor(Color.GRAY);
-                    g.fillRect((int) centroids[i].getValue(0) - 4, (int) centroids[i].getValue(1) - 4, 8, 8);
-
-                }
-                try{
-                g.setColor(Color.BLACK);
-                g.fillRect((int) centroids[tmpI].getValue(0) - 4, (int) centroids[tmpI].getValue(1) - 4, 8, 8);
-                }catch(Exception e){
-                    System.err.println("Error with tmpI: "+tmpI);
-                }
-
-            }
+//            if (km != null) {
+//                Instance[] centroids = km.getCentroids();
+//                for (int i = 0; i < centroids.length; i++) {
+//                    g.setColor(Color.GRAY);
+//                    g.fillRect((int) centroids[i].getValue(0) - 4, (int) centroids[i].getValue(1) - 4, 8, 8);
+//
+//                }
+//                try{
+//                g.setColor(Color.BLACK);
+//                g.fillRect((int) centroids[tmpI].getValue(0) - 4, (int) centroids[tmpI].getValue(1) - 4, 8, 8);
+//                }catch(Exception e){
+//                    System.err.println("Error with tmpI: "+tmpI);
+//                }
+//
+//            }
 
         }
 
