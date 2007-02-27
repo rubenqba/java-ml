39be745144bc75bda8ffbbd83bb0678ff1ad2422
andreasderijcke <andreasderijcke@3162d2cb-3e1f-0410-96b0-914c58786302>
2007-02-27 16:49:55 +0000

    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@215 3162d2cb-3e1f-0410-96b0-914c58786302

--- net/sf/javaml/clustering/mcl/MCL.java
+++ net/sf/javaml/clustering/mcl/MCL.java
@@ -24,6 +24,8 @@
  */
 package net.sf.javaml.clustering.mcl;
 
+import java.util.Vector;
+
 import net.sf.javaml.clustering.Clusterer;
 import net.sf.javaml.core.Dataset;
 import net.sf.javaml.core.Instance;
@@ -31,24 +33,89 @@ import net.sf.javaml.core.Instance;
 // TODO implements bridge between Gregory's implementation and the interfaces.
 public class MCL implements Clusterer {
 
-    public void buildClusterer(Dataset data) {
-        // TODO Auto-generated method stub
-
-    }
-
-    public int predictCluster(Instance instance) {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-    public double[] predictMembershipDistribution(Instance instance) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    public int getNumberOfClusters() {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
+	public Dataset[] executeClustering(Dataset data) {
+		// TODO convert dataset to matrix of distances
+		MarkovClustering mcl = new MarkovClustering();
+		SparseMatrix matrix = mcl.run(dataConverted, maxResidual, pGamma,
+				loopGain, maxZero);
+		
+		// convert matrix to output dataset:
+		
+		Vector<double[]> attractors = new Vector<double[]>();
+		// find attractors in diagonal
+		for (int i = 0; i < data.size(); i++) {
+			// attractor[0][1]: [0] = index in dataset/matrix, [1]= value in
+			// matrix.
+			double[] attractor = new double[2];
+			double val = matrix.get(i, i);
+			if (val != 0) {
+				attractor[0] = i;
+				attractor[1] = val;
+				attractors.add(attractor);
+			}
+		}
+		// search for the different attractors values, their number is resulting
+		// number of clusters.
+		Vector<Double> attractorvalues = new Vector<Double>();
+		double temp = attractors.get(0)[1];
+		attractorvalues.add(temp);
+		for (int i = 1; i < attractors.size(); i++) {
+			double temp2 = attractors.get(i)[1];
+			for (int j = 0; j < attractorvalues.size(); j++) {
+				double temp3 = attractors.get(j)[1];
+				if (temp2 != temp3) {
+					attractorvalues.add(temp3);
+				}
+			}
+		}
+		// put index of attractors with same value together. = attractorsystem
+		Vector<Double> attractorIndex = new Vector<Double>();
+		Vector<Vector<Double>> attractorSystems = new Vector<Vector<Double>>();
+		for (int i =0; i< attractorvalues.size();i++){
+			for (int j = 0; j < attractors.size();j++ ){
+				if ( attractors.get(j)[1]== attractorvalues.get(i)){
+					attractorIndex.add(attractors.get(j)[1]);
+				}
+			}
+			attractorSystems.add(attractorIndex);
+		}
+		// cluster consist of an attractorsystem and the data they attract.
+		// for each attractorsystem, find attracted data and add to cluster
+		Vector<Instance> cluster= new Vector<Instance>();
+		Vector<Vector<Instance>> finalClusters = new Vector<Vector<Instance>>();
+		// for each attractorsystem
+		for (int i =0; i< attractorSystems.size();i++){
+			// get columnindexes
+			attractorIndex = attractorSystems.get(i);
+			// for each column
+			for (int j = 0; j < attractorIndex.size();j++ ){
+				int value = attractorIndex.get(j).intValue();
+				// add attractor to cluster
+				cluster.add(data.getInstance(value));
+				// find data attracted to attractor (when matrix value is not 0)
+				// and add to cluster if not already present
+				for (int k = 0; k < data.size(); k++){
+					if( matrix.get(value,k) != 0){
+						for(int l = 0; l<cluster.size();l++){
+							if(data.getInstance(k) !=  cluster.get(j)){
+								cluster.add(data.getInstance(k));
+							}		
+						}
+						
+					}
+				}	
+			}
+			finalClusters.add(cluster);
+		}
+		// convert cluster
+		Dataset[] output = new Dataset[finalClusters.size()];
+		for (int i = 0; i < finalClusters.size(); i++) {
+			Vector<Instance> getCluster = new Vector<Instance>();
+			getCluster = finalClusters.get(i);
+			for (int j = 0; j < getCluster.size(); j++) {
+				output[i].addInstance(getCluster.get(j));
+			}
+		}
+		return output;
+	}
 }
