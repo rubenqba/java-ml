f95b2eaf5806d7b00faae70712d7533326d4a432
andreasderijcke <andreasderijcke@3162d2cb-3e1f-0410-96b0-914c58786302>
2007-03-13 12:45:22 +0000

    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@270 3162d2cb-3e1f-0410-96b0-914c58786302

--- net/sf/javaml/test/VisualTestAQBC.java
+++ net/sf/javaml/test/VisualTestAQBC.java
@@ -62,7 +62,7 @@ public class VisualTestAQBC  extends JPanel {
     public VisualTestAQBC() {
         this.setLayout(new GridLayout(0, 3));
         int space = 300;
-        Dataset data = DatasetGenerator.createClusterSquareDataset3D(space, 10,50);
+        Dataset data = DatasetGenerator.createClusterSquareDataset3D(space, 10,100);
 
         this.add(createLabel(data, Color.BLACK, space, space, null, null, 0));
         AdaptiveQualityBasedClustering km = new AdaptiveQualityBasedClustering();
