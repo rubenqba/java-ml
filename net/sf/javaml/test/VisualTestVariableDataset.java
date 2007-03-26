25598b19be6240a4a922640f7076b3d2f321af29
thomasabeel <thomasabeel@3162d2cb-3e1f-0410-96b0-914c58786302>
2007-03-26 12:40:50 +0000

    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@351 3162d2cb-3e1f-0410-96b0-914c58786302

--- net/sf/javaml/test/VisualTestVariableDataset.java
+++ net/sf/javaml/test/VisualTestVariableDataset.java
@@ -40,68 +40,62 @@ import net.sf.javaml.clustering.Ant;
 import net.sf.javaml.core.Dataset;
 import net.sf.javaml.core.Instance;
 import net.sf.javaml.test.VisualTestAnt.ClusterLabel;
-import net.sf.javaml.tools.DatasetGenerator;
 import net.sf.javaml.data.DatasetLoader;
 
 public class VisualTestVariableDataset extends JPanel {
-	private static final long serialVersionUID = 5608683822417234316L;
+    private static final long serialVersionUID = 5608683822417234316L;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) throws Exception {
-		JFrame window = new JFrame("Dataset visual test");
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setContentPane(new VisualTestVariableDataset());
-		window.pack();
-		window.setVisible(true);
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws Exception {
+        JFrame window = new JFrame("Dataset visual test");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setContentPane(new VisualTestVariableDataset());
+        window.pack();
+        window.setVisible(true);
 
-	}
+    }
 
-	public VisualTestVariableDataset() throws Exception {
-		this.setLayout(new GridLayout(0, 5));
-		int space = 200;
-		// Dataset data = DatasetGenerator.createClusterSquareDataset(space, 8,
-		// 50);
-		Dataset data = DatasetLoader
-				.loadDataset(new File("clustersize25.data"));
-		this.add(createLabel(data, Color.BLACK, space, space, null, null, 0));
+    int space = 750;
 
-	}
+    public VisualTestVariableDataset() throws Exception {
+        // Dataset data = DatasetGenerator.createClusterSquareDataset(space, 8,
+        // 50);
+        Dataset data = DatasetLoader.loadDataset(new File("clustersize1600.data"));
+        this.add(createLabel(data));
 
-	 private JLabel createLabel(Dataset data, Color color, int width, int height, Ant km, Color[] colors, int i) {
-	        return new ClusterLabel(data, color, width, height, km, colors, i);
-	    }
+    }
 
-	class ClusterLabel extends JLabel {
+    private JLabel createLabel(Dataset data) {
+        return new ClusterLabel(data);
+    }
 
-		public void setData(Dataset x) {
-			this.data = x;
-		}
+    class ClusterLabel extends JLabel {
 
-		private static final long serialVersionUID = -5642750374875570050L;
+        public void setData(Dataset x) {
+            this.data = x;
+        }
 
-		private Dataset data;
+        private static final long serialVersionUID = -5642750374875570050L;
 
-		private Color color;
+        private Dataset data;
 
-		public ClusterLabel(Dataset data, Color color, int width, int height,
-				Ant km, Color[] colors, int i) {
-			this.setPreferredSize(new Dimension(width, height));
-			this.data = data;
-			this.color = color;
-			this.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-		}
+        public ClusterLabel(Dataset data) {
+            this.setPreferredSize(new Dimension(space, space));
+            this.data = data;
+            this.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        }
 
-		@Override
-		protected void paintComponent(Graphics g) {
-			super.paintComponent(g);
-			g.setColor(color);
-			for (int i = 0; i < data.size(); i++) {
-				Instance in = data.getInstance(i);
-				g.fillOval((int) in.getValue(0) - 1, (int) in.getValue(1) - 1,
-						2, 2);
-			}
-		}
-	}
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.setColor(Color.black);
+            for (int i = 0; i < data.size(); i++) {
+                Instance in = data.getInstance(i);
+                System.out.println(in.getValue(0) * space + "\t" + in.getValue(1) * space);
+                g.fillOval((int) (in.getValue(0) * space), (int) (in.getValue(1) * space), 2, 2);
+            }
+        }
+    }
 }
