cc9d0e573074406de97ead800a9ec7f24106ec14
andreasderijcke <andreasderijcke@3162d2cb-3e1f-0410-96b0-914c58786302>
2007-03-26 12:22:25 +0000

    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@350 3162d2cb-3e1f-0410-96b0-914c58786302

--- net/sf/javaml/test/VisualTestVariableDataset.java
+++ net/sf/javaml/test/VisualTestVariableDataset.java
@@ -39,8 +39,9 @@ import javax.swing.JPanel;
 import net.sf.javaml.clustering.Ant;
 import net.sf.javaml.core.Dataset;
 import net.sf.javaml.core.Instance;
+import net.sf.javaml.test.VisualTestAnt.ClusterLabel;
 import net.sf.javaml.tools.DatasetGenerator;
-import net.sf.javaml.tools.DatasetLoader;
+import net.sf.javaml.data.DatasetLoader;
 
 public class VisualTestVariableDataset extends JPanel {
 	private static final long serialVersionUID = 5608683822417234316L;
@@ -48,57 +49,59 @@ public class VisualTestVariableDataset extends JPanel {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args)throws Exception {
-	        JFrame window = new JFrame("Dataset visual test");
-	        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        window.setContentPane(new VisualTestVariableDataset());
-	        window.pack();
-	        window.setVisible(true);
+	public static void main(String[] args) throws Exception {
+		JFrame window = new JFrame("Dataset visual test");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setContentPane(new VisualTestVariableDataset());
+		window.pack();
+		window.setVisible(true);
 
-	    }
+	}
 
 	public VisualTestVariableDataset() throws Exception {
 		this.setLayout(new GridLayout(0, 5));
 		int space = 200;
-		Dataset data = DatasetGenerator.createClusterSquareDataset(space, 8, 50);
-		//Dataset data = DatasetLoader.loadDataset(new File("clustersize25.data"));
-		this.add(createLabel(data, Color.BLACK, space, space, 0));
-		
+		// Dataset data = DatasetGenerator.createClusterSquareDataset(space, 8,
+		// 50);
+		Dataset data = DatasetLoader
+				.loadDataset(new File("clustersize25.data"));
+		this.add(createLabel(data, Color.BLACK, space, space, null, null, 0));
+
 	}
 
-	 private JLabel createLabel(Dataset data, Color color, int width, int height, int i) {
-	        return new ClusterLabel(data, color, width, height,i);
+	 private JLabel createLabel(Dataset data, Color color, int width, int height, Ant km, Color[] colors, int i) {
+	        return new ClusterLabel(data, color, width, height, km, colors, i);
 	    }
 
-	    class ClusterLabel extends JLabel {
+	class ClusterLabel extends JLabel {
 
-	        public void setData(Dataset x) {
-	            this.data = x;
-	        }
+		public void setData(Dataset x) {
+			this.data = x;
+		}
 
-	        private static final long serialVersionUID = -5642750374875570050L;
+		private static final long serialVersionUID = -5642750374875570050L;
 
-	        private Dataset data;
+		private Dataset data;
 
-	        private Color color;
+		private Color color;
 
-	       
-	        public ClusterLabel(Dataset data, Color color, int width, int height, int i) {
-	            this.setPreferredSize(new Dimension(width, height));
-	            this.data = data;
-	            this.color = color;
-	            this.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-	        }
-	        @Override
-	        protected void paintComponent(Graphics g) {
-	            super.paintComponent(g);
-	            g.setColor(color);
-	            for (int i = 0; i < data.size(); i++) {
-	                Instance in = data.getInstance(i);
-	                g.fillOval((int) in.getValue(0) - 1, (int) in.getValue(1) - 1, 2, 2);
+		public ClusterLabel(Dataset data, Color color, int width, int height,
+				Ant km, Color[] colors, int i) {
+			this.setPreferredSize(new Dimension(width, height));
+			this.data = data;
+			this.color = color;
+			this.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		}
 
-	            }
-	        }
+		@Override
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			g.setColor(color);
+			for (int i = 0; i < data.size(); i++) {
+				Instance in = data.getInstance(i);
+				g.fillOval((int) in.getValue(0) - 1, (int) in.getValue(1) - 1,
+						2, 2);
+			}
+		}
 	}
-
 }
