388fdb41bd632f07aeb49194b5bd0b06bb05d0d5
andreasderijcke <andreasderijcke@3162d2cb-3e1f-0410-96b0-914c58786302>
2007-04-11 11:51:50 +0000

    use of vector removed, arrays instead
    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@464 3162d2cb-3e1f-0410-96b0-914c58786302

--- net/sf/javaml/clustering/evaluation/TraceScatterMatrix.java
+++ net/sf/javaml/clustering/evaluation/TraceScatterMatrix.java
@@ -25,8 +25,6 @@
  */
 package net.sf.javaml.clustering.evaluation;
 
-import java.util.Vector;
-
 import net.sf.javaml.core.Dataset;
 import net.sf.javaml.core.DatasetTools;
 import net.sf.javaml.core.Instance;
@@ -59,16 +57,14 @@ public class TraceScatterMatrix implements ClusterEvaluation {
      * XXX DOC
      */
     public double score(Dataset[] clusters) {
-        Instance clusterCentroid;
+        Instance [] clusterCentroid= new Instance[clusters.length];
         Instance overAllCentroid;
-        Vector<Instance> clusterCentroids = new Vector<Instance>();
-        Vector<Integer> clusterSizes = new Vector<Integer>();
+        int [] clusterSizes = new int[clusters.length];
        
         // calculate centroids of each cluster
         for (int i = 0; i < clusters.length; i++) {
-            clusterCentroid = DatasetTools.getCentroid(clusters[i], new EuclideanDistance());
-            clusterCentroids.add(clusterCentroid);
-            clusterSizes.add(clusters[i].size());
+            clusterCentroid[i] = DatasetTools.getCentroid(clusters[i], new EuclideanDistance());
+            clusterSizes[i]=clusters[i].size();
         }
 
         // calculate centroid all instances
@@ -82,9 +78,9 @@ public class TraceScatterMatrix implements ClusterEvaluation {
         overAllCentroid = DatasetTools.getCentroid(data, new EuclideanDistance());
         // calculate trace of the between-cluster scatter matrix.
         double sum = 0;
-        for (int i = 0; i < clusterCentroids.size(); i++) {
-            double cos = dm.calculateDistance(clusterCentroids.get(i), overAllCentroid);
-            sum += cos * clusterSizes.get(i);
+        for (int i = 0; i < clusters.length; i++) {
+            double cos = dm.calculateDistance(clusterCentroid[i], overAllCentroid);
+            sum += cos * clusterSizes[i];
         }
         return sum;
     }
