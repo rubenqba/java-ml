ba7bde4d9bb1105f0a3add394109abe067a0e8e5
thomasabeel <thomasabeel@3162d2cb-3e1f-0410-96b0-914c58786302>
2007-07-13 12:27:09 +0000

    cleaned code
    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@706 3162d2cb-3e1f-0410-96b0-914c58786302

--- net/sf/javaml/classification/evaluation/CrossValidation.java
+++ net/sf/javaml/classification/evaluation/CrossValidation.java
@@ -1,5 +1,5 @@
 /**
- * CrossValidation.java, 17-jan-2007
+ * CrossValidation.java
  *
  * This file is part of the Java Machine Learning API
  * 
@@ -40,14 +40,15 @@ public class CrossValidation {
 
     public CrossValidation(Classifier classifier) {
         this.classifier = classifier;
-  }
+    }
 
-    //TODO make really random
-    private Random rg=new Random(1);
-    private int positiveClassValue=-1;
+    private Random rg = new Random(System.currentTimeMillis());
+
+    private int positiveClassValue = -1;
+
     public PerformanceMeasure crossValidation(Dataset data, int positiveClassValue, int folds) {
         this.data = data;
-        this.positiveClassValue=positiveClassValue;
+        this.positiveClassValue = positiveClassValue;
         // split data in positive and negative samples
         Vector<Integer> positiveSamples = new Vector<Integer>();
         Vector<Integer> negativeSamples = new Vector<Integer>();
@@ -65,91 +66,84 @@ public class CrossValidation {
                     + "-fold cross validation on the dataset");
         }
 
-        Vector<Vector<Integer>>positiveSets=new Vector<Vector<Integer>>();
-        Vector<Vector<Integer>>negativeSets=new Vector<Vector<Integer>>();
-        for(int i=0;i<folds;i++){
+        Vector<Vector<Integer>> positiveSets = new Vector<Vector<Integer>>();
+        Vector<Vector<Integer>> negativeSets = new Vector<Vector<Integer>>();
+        for (int i = 0; i < folds; i++) {
             positiveSets.add(new Vector<Integer>());
             negativeSets.add(new Vector<Integer>());
         }
-        int index=0;
-        while(positiveSamples.size()>0){
+        int index = 0;
+        while (positiveSamples.size() > 0) {
             positiveSets.get(index).add(positiveSamples.remove(rg.nextInt(positiveSamples.size())));
             index++;
-            index%=positiveSets.size();
+            index %= positiveSets.size();
         }
-        index=0;
-        while(positiveSamples.size()>0){
+        index = 0;
+        while (positiveSamples.size() > 0) {
             positiveSets.get(index).add(positiveSamples.remove(rg.nextInt(positiveSamples.size())));
             index++;
-            index%=positiveSets.size();
+            index %= positiveSets.size();
         }
-        index=0;
-        while(negativeSamples.size()>0){
+        index = 0;
+        while (negativeSamples.size() > 0) {
             negativeSets.get(index).add(negativeSamples.remove(rg.nextInt(negativeSamples.size())));
             index++;
-            index%=negativeSets.size();
+            index %= negativeSets.size();
         }
-        int tp=0,tn=0,fp=0,fn=0;
-        for(int i=0;i<folds;i++){
-            PerformanceMeasure pm=singleFold(positiveSets,negativeSets,i);
-            //System.out.println("Fold "+i+":\t"+pm);
-            tp+=pm.truePositives;
-            tn+=pm.trueNegatives;
-            fp+=pm.falsePositives;
-            fn+=pm.falseNegatives;
+        int tp = 0, tn = 0, fp = 0, fn = 0;
+        for (int i = 0; i < folds; i++) {
+            PerformanceMeasure pm = singleFold(positiveSets, negativeSets, i);
+            // System.out.println("Fold " + i + ":\t" + pm);
+            tp += pm.truePositives;
+            tn += pm.trueNegatives;
+            fp += pm.falsePositives;
+            fn += pm.falseNegatives;
         }
-        
-        return new PerformanceMeasure(tp,tn,fp,fn);
+
+        return new PerformanceMeasure(tp, tn, fp, fn);
     }
-    
-    private PerformanceMeasure singleFold(Vector<Vector<Integer>> positiveSets, Vector<Vector<Integer>> negativeSets, int fold) {
-        Dataset training=new SimpleDataset();
-        for(int i=0;i<positiveSets.size();i++){
-            if(i!=fold){
-                Vector<Integer>tmpPos=positiveSets.get(i);
-                for(int j=0;j<tmpPos.size();j++){
+
+    private PerformanceMeasure singleFold(Vector<Vector<Integer>> positiveSets, Vector<Vector<Integer>> negativeSets,
+            int fold) {
+        Dataset training = new SimpleDataset();
+        for (int i = 0; i < positiveSets.size(); i++) {
+            if (i != fold) {
+                Vector<Integer> tmpPos = positiveSets.get(i);
+                for (int j = 0; j < tmpPos.size(); j++) {
                     training.addInstance(data.getInstance(tmpPos.get(j)));
                 }
-                
-                Vector<Integer>tmpNeg=negativeSets.get(i);
-                for(int j=0;j<tmpNeg.size();j++){
+
+                Vector<Integer> tmpNeg = negativeSets.get(i);
+                for (int j = 0; j < tmpNeg.size(); j++) {
                     training.addInstance(data.getInstance(tmpNeg.get(j)));
                 }
             }
         }
-       classifier.buildClassifier(training);
-        //System.out.println("Threshold="+((SMOPlatt)classifier).getThreshold());
-        //System.out.println("Support vectors="+((SMOPlatt)classifier).getNumSupportVectors());
-        
-        Vector<Integer> valPos=positiveSets.get(fold);
-        int tp=0,fn=0;
-        for(int i=0;i<valPos.size();i++){
-            Instance tmp=data.getInstance(valPos.get(i));
-            int value=classifier.classifyInstance(tmp);
-            //System.out.println("POS CLASS: "+value);
-            if(value==1&&tmp.getClassValue()==this.positiveClassValue)
+        classifier.buildClassifier(training);
+
+        Vector<Integer> valPos = positiveSets.get(fold);
+        int tp = 0, fn = 0;
+        for (int i = 0; i < valPos.size(); i++) {
+            Instance tmp = data.getInstance(valPos.get(i));
+            int value = classifier.classifyInstance(tmp);
+            if (value == 1 && tmp.getClassValue() == this.positiveClassValue)
                 tp++;
-            else{
+            else {
                 fn++;
             }
         }
-        int tn=0,fp=0;
-        Vector<Integer> valNeg=negativeSets.get(fold);
-        for(int i=0;i<valNeg.size();i++){
-            Instance tmp=data.getInstance(valNeg.get(i));
-            int value=classifier.classifyInstance(tmp);
-            //System.out.println("NEG CLASS: "+value);
-            if(value==0&&tmp.getClassValue()!=this.positiveClassValue){
+        int tn = 0, fp = 0;
+        Vector<Integer> valNeg = negativeSets.get(fold);
+        for (int i = 0; i < valNeg.size(); i++) {
+            Instance tmp = data.getInstance(valNeg.get(i));
+            int value = classifier.classifyInstance(tmp);
+            if (value == 0 && tmp.getClassValue() != this.positiveClassValue) {
                 tn++;
-            }
-            else{
+            } else {
                 fp++;
             }
         }
-        //System.out.println("Validation on "+(valNeg.size()+valPos.size())+" samples");
-        return new PerformanceMeasure(tp,tn,fp,fn);
+        return new PerformanceMeasure(tp, tn, fp, fn);
     }
-
-   
 
 }
