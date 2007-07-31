9057cb7b161d7d3014a2ef79a7947e0b5ae5d7b3
thomasabeel <thomasabeel@3162d2cb-3e1f-0410-96b0-914c58786302>
2007-07-31 12:30:41 +0000

    added an implementation of a Pair
    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@721 3162d2cb-3e1f-0410-96b0-914c58786302

---
+++ net/sf/javaml/core/Pair.java
@@ -0,0 +1,69 @@
+/**
+ * Pair.java
+ *
+ * This file is part of the Java Machine Learning API
+ * 
+ * The Java Machine Learning API is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * The Java Machine Learning API is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with the Java Machine Learning API; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ * Copyright (c) 2006-2007, Thomas Abeel
+ * 
+ * Project: http://sourceforge.net/projects/java-ml/
+ * 
+ */
+/**
+ * 
+ */
+package net.sf.javaml.core;
+
+/**
+ * Represents a pair or couple of objects.
+ * 
+ * @author Thomas Abeel
+ * 
+ * @param <S>
+ * @param <T>
+ */
+public class Pair<S, T> {
+
+    private S x;
+
+    private T y;
+
+    private int hashCode;
+    private String string;
+    
+    public Pair(S x, T y) {
+        this.x = x;
+        this.y = y;
+        this.string="[" + x.toString() + ";" + y.toString() + "]";
+        this.hashCode=x.hashCode() * y.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Pair p = (Pair) o;
+        return p.x.equals(this.x) && p.y.equals(this.y);
+    }
+
+    @Override
+    public int hashCode() {
+        return hashCode;
+    }
+
+    @Override
+    public String toString() {
+        return string;
+    }
+}
