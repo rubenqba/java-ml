a54a0d1e57eb1fb2f92ca9cdddd929c884e6a650
thomasabeel <thomasabeel@3162d2cb-3e1f-0410-96b0-914c58786302>
2007-10-22 15:27:43 +0000

    improved documentation
    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@771 3162d2cb-3e1f-0410-96b0-914c58786302

--- net/sf/javaml/core/Complex.java
+++ net/sf/javaml/core/Complex.java
@@ -1,20 +1,20 @@
 /**
  * Complex.java
  *
- * This file is part of the Java Machine Learning API
+ * This file is part of the Java Machine Learning Library
  * 
- * The Java Machine Learning API is free software; you can redistribute it and/or modify
+ * The Java Machine Learning Library is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * The Java Machine Learning API is distributed in the hope that it will be useful,
+ * The Java Machine Learning Library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with the Java Machine Learning API; if not, write to the Free Software
+ * along with the Java Machine Learning Library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  * 
  * Copyright (c) 2006-2007, Thomas Abeel
@@ -25,8 +25,7 @@
 package net.sf.javaml.core;
 
 /**
- * 
- * Implements a mutable Complex number
+ * Implements a mutable Complex number.
  * 
  * @author Thomas Abeel
  * 
@@ -35,11 +34,22 @@ public class Complex {
 
     public double im = 0, re = 0;
 
+    /**
+     * Creates a new Complex number with the supplied real and complex part.
+     * 
+     * @param re
+     *            the real part
+     * @param im
+     *            the complex part
+     */
     public Complex(double re, double im) {
         this.re = re;
         this.im = im;
     }
 
+    /**
+     * Creates a new Complex number with 0 for its real and imaginary part.
+     */
     public Complex() {
         this(0, 0);
     }
@@ -52,16 +62,19 @@ public class Complex {
     }
 
     /**
-     * returns |this|
+     * The absolute value of this complex number. Basically it return |this|.
+     * 
+     * @return the absolute value of this complex number
      */
     public double abs() {
         return Math.sqrt(re * re + im * im);
     }
 
     /**
-     * plus b
+     * Add a complex number to this one.
      * 
      * @param b
+     *            the number to add to this one.
      */
     public void plus(Complex b) {
         re += b.re;
@@ -69,9 +82,10 @@ public class Complex {
     }
 
     /**
-     * minus b
+     * Subtracts a complex number from this one.
      * 
      * @param b
+     *            the number to subtract from this one.
      */
     public void minus(Complex b) {
         re -= b.re;
@@ -79,7 +93,10 @@ public class Complex {
     }
 
     /**
-     * times b
+     * Multiply this complex number with another one.
+     * 
+     * @param b
+     *            the number to multiply with.
      */
     public void times(Complex b) {
         double real = re * b.re - im * b.im;
@@ -92,6 +109,7 @@ public class Complex {
      * Multiply with real number
      * 
      * @param alpha
+     *            the real number to multiply with.
      */
     public void times(double alpha) {
         re *= alpha;
@@ -99,7 +117,7 @@ public class Complex {
     }
 
     /**
-     * Conjugate
+     * Take the conjugate of this complex number.
      * 
      */
     public void conjugate() {
@@ -107,11 +125,13 @@ public class Complex {
     }
 
     /**
-     * Static plus
+     * Add two complex numbers and return the result.
      * 
      * @param a
+     *            first complex number
      * @param b
-     * @return
+     *            second complex number
+     * @return the sum of a and b
      */
     public static Complex plus(Complex a, Complex b) {
         double real = a.re + b.re;
@@ -122,12 +142,14 @@ public class Complex {
     public static Complex multiply(Complex a, double d) {
         return new Complex(a.re * d, a.im * d);
     }
+
     public static Complex multiply(Complex a, Complex b) {
-        //(x + yi)(u + vi) = (xu – yv) + (xv + yu)i.
+        // (x + yi)(u + vi) = (xu – yv) + (xv + yu)i.
         double real = a.re * b.re - a.im * b.im;
         double imag = a.re * b.im + a.im * b.re;
-        return new Complex(real,imag);
+        return new Complex(real, imag);
     }
-    public static final Complex I=new Complex(0,1);
+
+    public static final Complex I = new Complex(0, 1);
 
 }
