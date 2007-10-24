f5a48c926a53335427711ef2a675024d46900990
thomasabeel <thomasabeel@3162d2cb-3e1f-0410-96b0-914c58786302>
2007-10-24 15:32:48 +0000

    replaced full headers with the %SVN.HEADER% tag
    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@778 3162d2cb-3e1f-0410-96b0-914c58786302

--- net/sf/javaml/core/Dataset.java
+++ net/sf/javaml/core/Dataset.java
@@ -1,26 +1,7 @@
 /**
  * Dataset.java
  *
- * This file is part of the Java Machine Learning API
- * 
- * The Java Machine Learning API is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * The Java Machine Learning API is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with the Java Machine Learning API; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- * 
- * Copyright (c) 2006-2007, Thomas Abeel
- * 
- * Project: http://sourceforge.net/projects/java-ml/
- * 
+ * %SVN.HEADER%
  */
 
 package net.sf.javaml.core;
@@ -88,11 +69,12 @@ public interface Dataset extends Iterable<Instance> {
     public Instance getMaximumInstance();
 
     /**
-     * Returns the number of different classes in the dataset. 
+     * Returns the number of different classes in the dataset.
+     * 
      * @return
      */
     public int getNumClasses();
-    
+
     /**
      * Sorts the dataset according to the attribute with the given index.
      * 
--- net/sf/javaml/core/Verbose.java
+++ net/sf/javaml/core/Verbose.java
@@ -1,33 +1,14 @@
 /**
  * Verbose.java
  *
- * This file is part of the Java Machine Learning API
- * 
- * The Java Machine Learning API is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * The Java Machine Learning API is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with the Java Machine Learning API; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- * 
- * Copyright (c) 2006-2007, Thomas Abeel
- * 
- * Project: http://sourceforge.net/projects/java-ml/
- * 
+ * %SVN.HEADER%
  */
 package net.sf.javaml.core;
 
 import java.util.logging.Logger;
 
 public class Verbose {
-    
+
     private Logger log = Logger.getAnonymousLogger();
 
     private boolean verbose = false;
@@ -35,9 +16,9 @@ public class Verbose {
     public void setVerbose(boolean verbose) {
         this.verbose = verbose;
     }
-    
-    protected void verbose(String s){
-        if(verbose)
+
+    protected void verbose(String s) {
+        if (verbose)
             log.info(s);
     }
 }
