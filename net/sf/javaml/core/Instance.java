96a331fd5e5fc6ebfe00af527ccc96806d1e12d4
thomasabeel <thomasabeel@3162d2cb-3e1f-0410-96b0-914c58786302>
2007-10-24 16:12:57 +0000

    improved javadoc
    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@781 3162d2cb-3e1f-0410-96b0-914c58786302

--- net/sf/javaml/core/Instance.java
+++ net/sf/javaml/core/Instance.java
@@ -8,51 +8,85 @@ package net.sf.javaml.core;
 
 import java.io.Serializable;
 
+/**
+ * Provides an interface for all instances. An instance is a collection of
+ * attributes that belong together.
+ * 
+ * Instances can be grouped together into a {@link Dataset}.
+ * 
+ * {jmlSource}
+ * 
+ * @see Dataset
+ * 
+ * @author Thomas Abeel
+ * 
+ */
 public interface Instance extends Serializable {
 
     /**
-     * This method will convert the instance to a value array. If this is not
-     * supported by the implementation, <code>null</code> should be returned.
+     * Converts the instance to a value array. If this is not supported by the
+     * implementation, <code>null</code> should be returned.
      * 
-     * @return
+     * @return the attributes of this instance
      */
     @Deprecated
     public double[] toArray();
 
     /**
-     * This method returns the value at a given index. If the implementation
-     * does not support positional access, an arbitrary value can be returned.
+     * Returns the value at a given index. If the implementation does not
+     * support positional access, an arbitrary value can be returned.
      * 
      * @param index
-     * @return
+     *            the attribute index
+     * @return the value of the attribute on the supplied index
      */
     public double getValue(int index);
 
     /**
-     * This method returns the complex values at a given index.
+     * Returns the complex values at a given index.
      * 
      */
     public Complex getComplex(int index);
 
     /**
-     * This method return the class value of this instance. If the method
-     * <code>isClassMissing</code> returns true, the output of this method is
-     * not defined.
+     * Return the class value of this instance. If the method
+     * <code>isClassSet</code> returns false, the output of this method is not
+     * defined.
      * 
      * @return the class value of this instance
      */
     public int getClassValue();
 
+    /**
+     * Return whether the class of this instance is set.
+     * 
+     * @return true if the class is set, false in other cases
+     */
     public boolean isClassSet();
 
+    /**
+     * Returns whether this instance is compatible with the supplied instance.
+     * For two instances to be compatible they should have the same number of
+     * attributes. Preferably the attributes should also be of the same type.
+     * 
+     * @param i
+     *            the instance to check the compatibility with
+     * @return true if the two instances are compatible, false if they are
+     *         incompatible
+     */
     public boolean isCompatible(Instance i);
 
+    /**
+     * Returns the weight of this instance.
+     * 
+     * @return the weight of the instance
+     */
     public double getWeight();
 
     /**
-     * This method return the number of values (attributes) this instance has.
+     * This method return the number of attributes this instance has.
      * 
-     * @return the number of values
+     * @return the number of attributes
      */
     public int size();
 
