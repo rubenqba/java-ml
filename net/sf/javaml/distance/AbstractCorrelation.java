02f1a432cdaef13b188b68b05720db3083675561
thomasabeel <thomasabeel@3162d2cb-3e1f-0410-96b0-914c58786302>
2008-01-24 09:10:32 +0000

    new type of distance
    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@814 3162d2cb-3e1f-0410-96b0-914c58786302

---
+++ net/sf/javaml/distance/AbstractCorrelation.java
@@ -0,0 +1,31 @@
+/**
+ * AbstractCorrelation.java
+ *
+ * %SVN.HEADER%
+ */
+package net.sf.javaml.distance;
+
+/**
+ * Abstract super class for all correlation measures. A correlation measure
+ * quantifies the correlation between two instances. Values are in the range
+ * [-1,1]. One means perfect correlation, zero means no correlation and minus
+ * one means perfect inverse correlation.
+ * 
+ * @see net.sf.javaml.distance.DistanceMeasure
+ * @see net.sf.javaml.distance.AbstractSimilarity
+ * @see net.sf.javaml.distance.AbstractDistance
+ * 
+ * @{jmlSource}
+ * 
+ * @version %SVN.VERSION%
+ * 
+ * @author Thomas Abeel
+ * 
+ */
+public abstract class AbstractCorrelation implements DistanceMeasure {
+
+    public boolean compare(double x, double y) {
+        return Math.abs(x) > Math.abs(y);
+    }
+
+}
