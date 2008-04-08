49229a8a4f1c985116f6d0f82e9f8ae9fb9a7edc
thomasabeel <thomasabeel@3162d2cb-3e1f-0410-96b0-914c58786302>
2008-04-08 08:59:06 +0000

    added test
    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@907 3162d2cb-3e1f-0410-96b0-914c58786302

---
+++ junit/utils/TestArrayUtils.java
@@ -0,0 +1,22 @@
+/**
+ * TestArrayUtils.java
+ *
+ * %SVN.HEADER%
+ */
+package junit.utils;
+
+import java.util.Arrays;
+
+import net.sf.javaml.utils.ArrayUtils;
+
+import org.junit.Test;
+
+public class TestArrayUtils {
+
+    @Test
+    public void testArrayUtils(){
+        double[]arr={1,5,4,3,8,10,2,6};
+        int[]order=ArrayUtils.sort(arr);
+        System.out.println(Arrays.toString(order));
+    }
+}
