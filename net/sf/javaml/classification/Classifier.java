889aa3e5b1d384815a86a60a2e0a51ad3563e731
thomasabeel <thomasabeel@3162d2cb-3e1f-0410-96b0-914c58786302>
2008-06-06 10:44:20 +0000

    first sparse classifier
    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@909 3162d2cb-3e1f-0410-96b0-914c58786302

---
+++ net/sf/javaml/classification/Classifier.java
@@ -0,0 +1,52 @@
+/**
+ * Classifier.java
+ *
+ * %SVN.HEADER%
+ */
+package net.sf.javaml.classification;
+
+import java.io.Serializable;
+import java.util.Map;
+
+import net.sf.javaml.core.Dataset;
+import net.sf.javaml.core.Instance;
+
+/**
+ * Interface for all classifiers.
+ * 
+ * {@jmlSource}
+ * 
+ * @author Thomas Abeel
+ * 
+ */
+public interface Classifier extends Serializable {
+	/**
+	 * Create a classifier from the given data set.
+	 * 
+	 * @param data
+	 *            the data set to be used to create the classifier
+	 */
+	public void buildClassifier(Dataset data);
+
+	/**
+	 * Classify the instance according to this classifier.
+	 * 
+	 * @param instance
+	 *            the instance to be classified
+	 * @return the class to which this instance belongs or null if it doesn't
+	 *         belong to any of the known classes.
+	 */
+	public Object classifyInstance(Instance instance);
+
+	/**
+	 * Generate the membership distribution for this instance using this
+	 * classifier.
+	 * 
+	 * @param instance
+	 *            the instance to be classified
+	 * @return an array with membership degrees for all the various classes in
+	 *         the data set
+	 */
+	public Map<Object,Double> distributionForInstance(Instance instance);
+
+}
---
+++ net/sf/javaml/classification/KNearestNeighbors.java
@@ -0,0 +1,113 @@
+/**
+ * KNearestNeighbors.java
+ *
+ * %SVN.HEADER%
+ */
+package net.sf.javaml.classification;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import net.sf.javaml.core.Dataset;
+import net.sf.javaml.core.Instance;
+import net.sf.javaml.distance.NormDistance;
+
+/**
+ * Implementation of the K nearest neighbor (KNN) classification algorithm.
+ * 
+ * @author Thomas Abeel
+ * 
+ */
+public class KNearestNeighbors implements Classifier {
+
+    private static final long serialVersionUID = 1560149339188819924L;
+
+    private Dataset training;
+
+    private NormDistance euc = new NormDistance(2);
+
+    private int k = 5;
+
+    public KNearestNeighbors(int k) {
+        this.k = k;
+    }
+
+    @Override
+    public void buildClassifier(Dataset data) {
+        this.training = data;
+
+    }
+
+    @Override
+    public Object classifyInstance(Instance instance) {
+        Map<Object, Double> distribution = distributionForInstance(instance);
+        double max = 0;
+        Object out = null;
+        for (Object key : distribution.keySet()) {
+            if (distribution.get(key) > max) {
+                max = distribution.get(key);
+                out = key;
+            }
+        }
+        // System.out.println("pred: "+out);
+        // System.out.println("--");
+        
+        return out;
+    }
+
+    @Override
+    public Map<Object, Double> distributionForInstance(Instance instance) {
+        Set<Instance> neighbors = training.kNearest(k, euc, instance);
+//        System.out.println(neighbors);
+        HashMap<Object, Double> out = new HashMap<Object, Double>();
+//        System.out.println("classes: "+training.classes());
+        for (Object o : training.classes())
+            out.put(o, 0.0);
+        // double[] output = new double[training.classes().size()];
+       
+        for (Instance i : neighbors) {
+//            System.out.println("Class: "+i.classValue());
+            out.put(i.classValue(), out.get(i.classValue()) + 1);
+        }
+//        System.out.println("real: "+instance.classValue());
+//        System.out.println(out);
+//        
+        double min = k;
+        double max = 0;
+        for (Object key : out.keySet()) {
+            double val = out.get(key);
+            if (val > max)
+                max = val;
+            if (val < min)
+                min = val;
+        }
+        for (Object key : out.keySet()) {
+            out.put(key, (out.get(key) - min) / (max - min));
+        }
+       
+        return out;
+    }
+
+    // public void buildClassifier(Dataset data) {
+    //        
+    //
+    // }
+    //
+    // public Object classifyInstance(Instance<?> instance) {
+    // return ArrayUtils.maxIndex(distributionForInstance(instance));
+    // }
+    //
+    // public double[] distributionForInstance(Instance instance) {
+    // List<Instance> neighbors = DatasetTools.getNearestK(training, euc,
+    // instance, k);
+    // double[] output = new double[training.numClasses()];
+    // for (Instance i : neighbors) {
+    // output[i.classValue()]++;
+    // }
+    // ArrayUtils.normalize(output);
+    // return output;
+    //
+    // }
+
+}
