c2e070a02fdfcb2395f4b7b1ed920d3401cf95e6
thomasabeel <thomasabeel@3162d2cb-3e1f-0410-96b0-914c58786302>
2008-08-13 10:35:22 +0000

    dataloader tutorial
    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@956 3162d2cb-3e1f-0410-96b0-914c58786302

---
+++ junit/tools/TutorialDataLoader.java
@@ -0,0 +1,49 @@
+/**
+ * %SVN.HEADER%
+ */
+package junit.tools;
+
+import java.io.File;
+import java.io.IOException;
+
+import junit.framework.Assert;
+
+import net.sf.javaml.core.Dataset;
+import net.sf.javaml.tools.data.FileHandler;
+
+import org.junit.Test;
+
+/**
+ * This tutorial shows how to load data from a local file.
+ * 
+ * The two files that are used here can be retrieved from the SVN or at the
+ * following URLS:
+ * 
+ * {@linkplain http
+ * ://java-ml.svn.sourceforge.net/viewvc/java-ml/trunk/devtools/data/iris.data}
+ * and {@linkplain http
+ * ://java-ml.svn.sourceforge.net/viewvc/java-ml/trunk/devtools/data/sparse.tsv}
+ * .
+ * 
+ * Check out these two files for the dense and sparse file formats. The class
+ * label can have any value, the other attributes should be numbers.
+ * 
+ * @author Thomas Abeel
+ * 
+ */
+public class TutorialDataLoader {
+
+	@Test
+	public void testLoader() {
+		try {
+			Dataset data = FileHandler.loadDataset(new File(
+					"devtools/data/iris.data"), 4, ",");
+			System.out.println(data);
+			data = FileHandler.loadSparseDataset(new File(
+					"devtools/data/sparse.tsv"), 0, " ", ":");
+			System.out.println(data);
+		} catch (IOException e) {
+			Assert.assertFalse(true);
+		}
+	}
+}
