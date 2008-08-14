1faabc969cc543098b312d1b0681a4dedc9c0a7f
thomasabeel <thomasabeel@3162d2cb-3e1f-0410-96b0-914c58786302>
2008-08-14 09:07:16 +0000

    + docs
    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@976 3162d2cb-3e1f-0410-96b0-914c58786302

--- net/sf/javaml/classification/Classifier.java
+++ net/sf/javaml/classification/Classifier.java
@@ -18,33 +18,37 @@ import net.sf.javaml.core.Instance;
  * 
  */
 public interface Classifier extends Serializable {
-	/**
-	 * Create a classifier from the given data set.
-	 * 
-	 * @param data
-	 *            the data set to be used to create the classifier
-	 */
-	public void buildClassifier(Dataset data);
+    /**
+     * Create a classifier from the given data set.
+     * 
+     * @param data
+     *            the data set to be used to create the classifier
+     */
+    public void buildClassifier(Dataset data);
 
-	/**
-	 * Classify the instance according to this classifier.
-	 * 
-	 * @param instance
-	 *            the instance to be classified
-	 * @return the class to which this instance belongs or null if it doesn't
-	 *         belong to any of the known classes.
-	 */
-	public Object classify(Instance instance);
+    /**
+     * Classify the instance according to this classifier.
+     * 
+     * @param instance
+     *            the instance to be classified
+     * @return the class to which this instance belongs or null if it doesn't
+     *         belong to any of the known classes.
+     */
+    public Object classify(Instance instance);
 
-	/**
-	 * Generate the membership distribution for this instance using this
-	 * classifier.
-	 * 
-	 * @param instance
-	 *            the instance to be classified
-	 * @return an array with membership degrees for all the various classes in
-	 *         the data set
-	 */
-	public Map<Object,Double> classDistribution(Instance instance);
+    /**
+     * Generate the membership distribution for this instance using this
+     * classifier.
+     * 
+     * Note: The returned map may not contain a value for all classes that were
+     * present in the data set used for training. If the map does not contain a
+     * value, the value for that class equals zero.
+     * 
+     * @param instance
+     *            the instance to be classified
+     * @return an array with membership degrees for all the various classes in
+     *         the data set
+     */
+    public Map<Object, Double> classDistribution(Instance instance);
 
 }
