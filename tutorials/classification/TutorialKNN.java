e7b8b17558715c031180941b9ed6c13c4dbc44d4
thomasabeel <thomasabeel@3162d2cb-3e1f-0410-96b0-914c58786302>
2008-08-14 10:48:52 +0000

    knn test update
    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@983 3162d2cb-3e1f-0410-96b0-914c58786302

--- tutorials/classification/TutorialKNN.java
+++ tutorials/classification/TutorialKNN.java
@@ -15,6 +15,8 @@ import net.sf.javaml.tools.data.FileHandler;
 import org.junit.Assert;
 import org.junit.Test;
 
+import be.abeel.util.TimeInterval;
+
 /**
  * This tutorial show how to use a the k-nearest neighbors classifier.
  * 
@@ -22,47 +24,91 @@ import org.junit.Test;
  * 
  */
 public class TutorialKNN {
-	/**
-	 * Shows the default usage of the KNN algorithm.
-	 */
-	@Test
-	public void testDefaultKNN() {
+    /**
+     * Shows the default usage of the KNN algorithm.
+     */
+    @Test
+    public void testDefaultKNN() {
 
-		try {
-			/* Load a data set */
-			Dataset data = FileHandler.loadDataset(new File(
-					"devtools/data/iris.data"), 4, ",");
-			/*
-			 * Contruct a KNN classifier that uses 5 neighbors to make a
-			 * decision.
-			 */
-			Classifier knn = new KNearestNeighbors(5);
-			knn.buildClassifier(data);
+        try {
+            /* Load a data set */
+            Dataset data = FileHandler.loadDataset(new File("devtools/data/iris.data"), 4, ",");
+            /*
+             * Contruct a KNN classifier that uses 5 neighbors to make a
+             * decision.
+             */
+            Classifier knn = new KNearestNeighbors(5);
+            knn.buildClassifier(data);
 
-			/*
-			 * Load a data set, this can be a different one, but we will use the
-			 * same one.
-			 */
-			Dataset dataForClassification = FileHandler.loadDataset(new File(
-					"devtools/data/iris.data"), 4, ",");
-			/* Counters for correct and wrong predictions. */
-			int correct = 0, wrong = 0;
-			/* Classify all instances and check with the correct class values */
-			for (Instance inst : dataForClassification) {
-				Object predictedClassValue = knn.classify(inst);
-				Object realClassValue = inst.classValue();
-				if (predictedClassValue.equals(realClassValue))
-					correct++;
-				else
-					wrong++;
-			}
-			System.out.println("Correct predictions  " + correct);
-			System.out.println("Wrong predictions " + wrong);
+            /*
+             * Load a data set, this can be a different one, but we will use the
+             * same one.
+             */
+            Dataset dataForClassification = FileHandler.loadDataset(new File("devtools/data/iris.data"), 4, ",");
+            /* Counters for correct and wrong predictions. */
+            int correct = 0, wrong = 0;
+            /* Classify all instances and check with the correct class values */
+            for (Instance inst : dataForClassification) {
+                Object predictedClassValue = knn.classify(inst);
+                Object realClassValue = inst.classValue();
+                if (predictedClassValue.equals(realClassValue))
+                    correct++;
+                else
+                    wrong++;
+            }
+            System.out.println("Correct predictions  " + correct);
+            System.out.println("Wrong predictions " + wrong);
 
-		} catch (IOException e) {
-			Assert.assertTrue(false);
-		}
+        } catch (IOException e) {
+            Assert.assertTrue(false);
+        }
 
-	}
+    }
 
+    /**
+     * Test KNN usage on the sparse sample.
+     * 
+     */
+    @Test
+    public void testSparseKNN() {
+
+        try {
+            /* Load a data set */
+            Dataset data = FileHandler.loadSparseDataset(new File("devtools/data/sparse.txt.gz"), 0, ";", ":");
+            /*
+             * Contruct a KNN classifier that uses 5 neighbors to make a
+             * decision.
+             */
+            Classifier knn = new KNearestNeighbors(5);
+            knn.buildClassifier(data);
+            System.out.println("Building complete!");
+            /*
+             * Load a data set, this can be a different one, but we will use the
+             * same one.
+             */
+            Dataset dataForClassification = FileHandler.loadSparseDataset(new File("devtools/data/sparse.txt.gz"), 0,
+                    ";", ":");
+            /* Counters for correct and wrong predictions. */
+            int correct = 0, wrong = 0;
+            int count=0;
+            /* Classify all instances and check with the correct class values */
+            for (Instance inst : dataForClassification) {
+                long time=System.currentTimeMillis();
+                System.out.print("Processing instance: " + ++count+"\t");
+                Object predictedClassValue = knn.classify(inst);
+                Object realClassValue = inst.classValue();
+                if (predictedClassValue.equals(realClassValue))
+                    correct++;
+                else
+                    wrong++;
+                System.out.println(new TimeInterval(System.currentTimeMillis()-time));
+            }
+            System.out.println("Correct predictions  " + correct);
+            System.out.println("Wrong predictions " + wrong);
+
+        } catch (IOException e) {
+            Assert.assertTrue(false);
+        }
+
+    }
 }
