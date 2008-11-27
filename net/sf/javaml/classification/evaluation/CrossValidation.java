836fd0b4f7cd596e473451b30b740edaf5106899
thomasabeel <thomasabeel@3162d2cb-3e1f-0410-96b0-914c58786302>
2008-11-27 13:10:09 +0000

    added convenience method, removed commented source and added documentation
    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@1053 3162d2cb-3e1f-0410-96b0-914c58786302

--- net/sf/javaml/classification/evaluation/CrossValidation.java
+++ net/sf/javaml/classification/evaluation/CrossValidation.java
@@ -31,20 +31,26 @@ public class CrossValidation {
         this.classifier = classifier;
     }
 
-    // private PerformanceMeasure[] foldPerformance = null;
-    // TODO use evaluateDataset
+    /**
+     * Performs cross validation with the specified parameters.
+     * 
+     * @param data
+     *            the data set to use in the cross validation. This data set is
+     *            split in the appropriate number of folds.
+     * @param numFolds
+     *            the number of folds to create
+     * @param rg
+     *            random generator to create the folds
+     * @return the results of the cross-validation.
+     */
     public Map<Object, PerformanceMeasure> crossValidation(Dataset data, int numFolds, Random rg) {
-        // System.out.println("CV:" + data.size());
+        //TODO use EvaluateDataset
         Dataset[] folds = data.folds(numFolds, rg);
-        // for(Dataset d:folds){
-        // System.out.println("\t"+d.size());
-        // }
         Map<Object, PerformanceMeasure> out = new HashMap<Object, PerformanceMeasure>();
         for (Object o : data.classes()) {
             out.put(o, new PerformanceMeasure());
         }
         for (int i = 0; i < numFolds; i++) {
-            // System.out.println("Fold: "+i);
             Dataset validation = folds[i];
             Dataset training = new DefaultDataset();
             for (int j = 0; j < numFolds; j++) {
@@ -52,26 +58,18 @@ public class CrossValidation {
                     training.addAll(folds[j]);
 
             }
-            // System.out.println("training: "+training.classes());
-            // System.out.println("validation: "+validation.classes());
-            // System.out.println("\tTraining:\t"+training.size());
-            // System.out.println("\tValidation\t"+validation.size());
+
             classifier.buildClassifier(training);
 
-            // int tp = 0, tn = 0, fn = 0, fp = 0;
-            // System.out.println("real
-            // class\tprediction\tpositive\tclassification");
             for (Instance instance : validation) {
                 Object prediction = classifier.classify(instance);
                 if (instance.classValue().equals(prediction)) {// prediction
                     // ==class
                     for (Object o : out.keySet()) {
                         if (o.equals(instance.classValue())) {
-                            // System.out.println(instance.classValue()+"\t"+prediction+"\t"+o+"\tTP");
                             out.get(o).tp++;
                         } else {
                             out.get(o).tn++;
-                            // System.out.println(instance.classValue()+"\t"+prediction+"\t"+o+"\tTN");
                         }
 
                     }
@@ -80,17 +78,14 @@ public class CrossValidation {
                         /* prediction is positive class */
                         if (prediction.equals(o)) {
                             out.get(o).fp++;
-                            // System.out.println(instance.classValue()+"\t"+prediction+"\t"+o+"\tFP");
                         }
                         /* instance is positive class */
                         else if (o.equals(instance.classValue())) {
                             out.get(o).fn++;
-                            // System.out.println(instance.classValue()+"\t"+prediction+"\t"+o+"\tFN");
                         }
                         /* none is positive class */
                         else {
                             out.get(o).tn++;
-                            // System.out.println(instance.classValue()+"\t"+prediction+"\t"+o+"\tTN");
                         }
 
                     }
@@ -99,6 +94,21 @@ public class CrossValidation {
 
         }
         return out;
+
+    }
+
+    /**
+     * Performs cross validation with the specified parameters.
+     * 
+     * @param data
+     *            the data set to use in the cross validation. This data set is
+     *            split in the appropriate number of folds.
+     * @param numFolds
+     *            the number of folds to create
+     * @return the results of the cross-validation.
+     */
+    public Map<Object, PerformanceMeasure> crossValidation(Dataset data, int folds) {
+        return crossValidation(data, folds, new Random(System.currentTimeMillis()));
 
     }
 
