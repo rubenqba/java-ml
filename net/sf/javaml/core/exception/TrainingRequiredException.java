204cdc6112e69f5ea6bb08662be2ac0a05596876
thomasabeel <thomasabeel@3162d2cb-3e1f-0410-96b0-914c58786302>
2008-11-27 14:23:37 +0000

    +doc
    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@1059 3162d2cb-3e1f-0410-96b0-914c58786302

--- net/sf/javaml/core/exception/TrainingRequiredException.java
+++ net/sf/javaml/core/exception/TrainingRequiredException.java
@@ -1,15 +1,18 @@
 /**
- * TrainingRequiredException.java
- *
  * %SVN.HEADER%
  */
 package net.sf.javaml.core.exception;
 
+/**
+ * Indicates that the algorithm that throws this exception should have been
+ * trained prior to point the exception was thrown. Typically this implies that
+ * the build method of a particular algorithm has not yet been called.
+ * 
+ * @author Thomas Abeel
+ * 
+ */
 public class TrainingRequiredException extends RuntimeException {
 
-    /**
-     * 
-     */
     private static final long serialVersionUID = 1774207131386358008L;
 
     public TrainingRequiredException() {
