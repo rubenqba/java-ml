1240f32457094c3d57124b9ad1d980ba5ba34b2b
thomasabeel <thomasabeel@3162d2cb-3e1f-0410-96b0-914c58786302>
2008-12-05 12:30:13 +0000

    added test for KMeans
    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@1076 3162d2cb-3e1f-0410-96b0-914c58786302

---
+++ junit/clustering/TestKMeans.java
@@ -0,0 +1,72 @@
+/**
+ * %SVN.HEADER%
+ */
+package junit.clustering;
+
+import java.io.File;
+import java.io.IOException;
+
+import net.sf.javaml.clustering.Clusterer;
+import net.sf.javaml.clustering.KMeans;
+import net.sf.javaml.core.Dataset;
+import net.sf.javaml.distance.EuclideanDistance;
+import net.sf.javaml.tools.data.FileHandler;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestKMeans {
+    /**
+     * Tests the k-means algorithm with default parameter settings.
+     */
+    @Test
+    public void testKMean() {
+        try {
+            /* Load a dataset */
+            Dataset data = FileHandler.loadDataset(new File(
+                    "devtools/data/iris.data"), 4, ",");
+            /*
+             * Create a new instance of the KMeans algorithm, with no options
+             * specified. By default this will generate 4 clusters.
+             */
+            Clusterer km = new KMeans();
+            /*
+             * Cluster the data, it will be returned as an array of data sets,
+             * with each dataset representing a cluster
+             */
+            Dataset[] clusters = km.cluster(data);
+            System.out.println("Cluster count: " + clusters.length);
+
+        } catch (IOException e) {
+            Assert.assertTrue(false);
+        }
+    }
+
+    /**
+     * Tests the k-means algorithm with user-specified parameter settings.
+     */
+    @Test
+    public void testKMeanWithParameters() {
+        try {
+            /* Load a dataset */
+            Dataset data = FileHandler.loadDataset(new File(
+                    "devtools/data/iris.data"), 4, ",");
+            /*
+             * Create a new instance of the KMeans algorithm, with all options
+             * specified. This instance of the k-means algorithm will generate 3
+             * clusters, will run for 100 iteration and will use the euclidean
+             * distance.
+             */
+            Clusterer km = new KMeans(3, 100, new EuclideanDistance());
+            /*
+             * Cluster the data, it will be returned as an array of data sets,
+             * with each data set representing a cluster
+             */
+            Dataset[] clusters = km.cluster(data);
+            System.out.println("Cluster count: " + clusters.length);
+
+        } catch (IOException e) {
+            Assert.assertTrue(false);
+        }
+    }
+}
