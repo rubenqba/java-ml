6040763b04634f153dfbd34c5b08a42276558872
thomasabeel <thomasabeel@3162d2cb-3e1f-0410-96b0-914c58786302>
2009-01-15 13:10:07 +0000

    +doc
    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@1125 3162d2cb-3e1f-0410-96b0-914c58786302

--- net/sf/javaml/core/Sets.java
+++ net/sf/javaml/core/Sets.java
@@ -6,18 +6,41 @@ package net.sf.javaml.core;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Implements additional operations on sets.
+ * 
+ * @author Thomas Abeel
+ * 
+ */
 public class Sets {
-
-    public static  Set<Integer> union(Set<? extends Integer> a, Set<? extends Integer> b) {
-        Set<Integer>out=new HashSet<Integer>();
+    /**
+     * Returns the union of the two sets provided as arguments.
+     * 
+     * @param a
+     *            the first set
+     * @param b
+     *            the second set
+     * @return union of a and b
+     */
+    public static Set<Integer> union(Set<? extends Integer> a, Set<? extends Integer> b) {
+        Set<Integer> out = new HashSet<Integer>();
         out.addAll(a);
         out.addAll(b);
         return out;
 
     }
-    
-    public static  Set<Integer> intersection(Set<? extends Integer> a, Set<? extends Integer> b) {
-        Set<Integer>out=new HashSet<Integer>();
+
+    /**
+     * Returns the intersection of the two sets provided as arguments.
+     * 
+     * @param a
+     *            the first set
+     * @param b
+     *            the second set
+     * @return intersection of a and b
+     */
+    public static Set<Integer> intersection(Set<? extends Integer> a, Set<? extends Integer> b) {
+        Set<Integer> out = new HashSet<Integer>();
         out.addAll(a);
         out.retainAll(b);
         return out;
