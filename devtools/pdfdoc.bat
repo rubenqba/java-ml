5380b9e99c5efa85bab934cf3550832493876712
thomasabeel <thomasabeel@3162d2cb-3e1f-0410-96b0-914c58786302>
2009-01-16 12:40:00 +0000

    updated release check, added pdfgeneration script
    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@1147 3162d2cb-3e1f-0410-96b0-914c58786302

---
+++ devtools/pdfdoc.bat
@@ -0,0 +1,6 @@
+"C:\Program Files\Mozilla Firefox\firefox" -print http://java-ml.sourceforge.net/print/book/export/html/57 -printmode pdf -printfile gettingstarted.pdf
+"C:\Program Files\Mozilla Firefox\firefox" -print http://java-ml.sourceforge.net/print/book/export/html/72 -printmode pdf -printfile clustering.pdf
+"C:\Program Files\Mozilla Firefox\firefox" -print http://java-ml.sourceforge.net/print/book/export/html/71 -printmode pdf -printfile featureselection.pdf
+"C:\Program Files\Mozilla Firefox\firefox" -print http://java-ml.sourceforge.net/print/book/export/html/65 -printmode pdf -printfile classification.pdf
+"C:\Program Files\Mozilla Firefox\firefox" -print http://java-ml.sourceforge.net/print/book/export/html/77 -printmode pdf -printfile datasets.pdf
+"C:\Program Files\Mozilla Firefox\firefox" -print http://java-ml.sourceforge.net/print/book/export/html/67 -printmode pdf -printfile developerdocs.pdf
