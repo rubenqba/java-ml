be5d9c2b30f4f6f2181e975b7d5c90e7e17abc4c
thomasabeel <thomasabeel@3162d2cb-3e1f-0410-96b0-914c58786302>
2009-03-19 18:07:37 +0000

    added RandomTree test
    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@1159 3162d2cb-3e1f-0410-96b0-914c58786302

--- junit/classification/TestRandomTree.java
+++ junit/classification/TestRandomTree.java
@@ -10,40 +10,44 @@ import java.util.Random;
 import net.sf.javaml.classification.evaluation.CrossValidation;
 import net.sf.javaml.classification.tree.RandomTree;
 import net.sf.javaml.core.Dataset;
+import net.sf.javaml.core.DefaultDataset;
+import net.sf.javaml.core.DenseInstance;
 import net.sf.javaml.tools.data.FileHandler;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 public class TestRandomTree {
-//    @Test
-//    public void testRTSparse() {
-//        try {
-//            Dataset data = FileHandler.loadSparseDataset(new File("devtools/data/sparse.txt.gz"), 0, ";", ":");
-//            System.out.println("noAttributes: " + data.noAttributes());
-//            System.out.println("instances: " + data.size());
-//            RandomTree rt = new RandomTree(5, new Random());
-//            CrossValidation cv = new CrossValidation(rt);
-//            System.out.println(cv.crossValidation(data, 2, new Random()));
-//        } catch (IOException e) {
-//            Assert.assertTrue(false);
-//        }
-//    }
+	@Test
+	public void testRFDestructiveConstruction() {
+		// Show that a data set can be modified during training
+		Dataset data = new DefaultDataset();
+		data.add(new DenseInstance(new double[] { 1, 2 }, "hallo"));
+		data.add(new DenseInstance(new double[] { 4, 2 }, "hallo"));
+		data.add(new DenseInstance(new double[] { 1, 1 }, "hallo"));
+		System.out.println("Loader: " + data.classes());
+		RandomTree rt = new RandomTree(1, new Random());
+		rt.buildClassifier(data);
+		Assert.assertEquals(0, data.size());
 
-    @Test
-    public void testRT() {
+	}
 
-        try {
-            Dataset data = FileHandler.loadDataset(new File("devtools/data/colon.csv.gz"), 0, ",");
-            System.out.println("Loader: " + data.classes());
-            RandomTree knn = new RandomTree(5, new Random());
-            CrossValidation cv = new CrossValidation(knn);
-            System.out.println("Java-ML-0:" + cv.crossValidation(data, 5, new Random(10)));
+	@Test
+	public void testRT() {
 
-        } catch (IOException e) {
-            Assert.assertTrue(false);
-        }
+		try {
+			Dataset data = FileHandler.loadDataset(new File(
+					"devtools/data/colon.csv.gz"), 0, ",");
+			System.out.println("Loader: " + data.classes());
+			RandomTree knn = new RandomTree(5, new Random());
+			CrossValidation cv = new CrossValidation(knn);
+			System.out.println("Java-ML-0:"
+					+ cv.crossValidation(data, 5, new Random(10)));
 
-    }
+		} catch (IOException e) {
+			Assert.assertTrue(false);
+		}
+
+	}
 
 }
