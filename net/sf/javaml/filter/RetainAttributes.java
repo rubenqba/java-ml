feddeae294451da091eecd8fc46d208514089baa
thomasabeel <thomasabeel@3162d2cb-3e1f-0410-96b0-914c58786302>
2009-05-04 18:22:01 +0000

    Fixed documentation and added constructor that takes a set as parameter
    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@1183 3162d2cb-3e1f-0410-96b0-914c58786302

--- net/sf/javaml/filter/RetainAttributes.java
+++ net/sf/javaml/filter/RetainAttributes.java
@@ -17,37 +17,48 @@ import net.sf.javaml.core.Instance;
  */
 public class RetainAttributes extends AbstractFilter {
 
-    private Set<Integer> toKeep = new HashSet<Integer>();
+	private Set<Integer> toKeep = new HashSet<Integer>();
 
-    private Set<Integer> toRemove = new HashSet<Integer>();
+	private Set<Integer> toRemove = new HashSet<Integer>();
 
-    /**
-     * Construct a filter that retains all the attributes with the indices given
-     * in the array as parameter.
-     * 
-     * @param indices
-     *            the indices of the columns that will be removed.
-     */
-    public RetainAttributes(int[] indices) {
-        for (int i : indices) {
-            this.toKeep.add(i);
-        }
-    }
+	/**
+	 * Construct a filter that retains all the attributes with the indices given
+	 * in the array as parameter.
+	 * 
+	 * @param indices
+	 *            the indices of the columns that will be retained.
+	 */
+	public RetainAttributes(int[] indices) {
+		for (int i : indices) {
+			this.toKeep.add(i);
+		}
+	}
 
-    @Override
-    public void filter(Instance instance) {
-        if (toRemove.size() + toKeep.size() != instance.noAttributes())
-            buildRemove(instance.noAttributes());
-        instance.removeAttributes(toRemove);
-    }
+	/**
+	 * Construct a filter that retains all the attributes with the indices given
+	 * in the array as parameter.
+	 * 
+	 * @param indices
+	 *            the indices of the columns that will be retained.
+	 */
+	public RetainAttributes(Set<Integer> indices) {
+		this.toKeep.addAll(indices);
+	}
 
-    private void buildRemove(int noAttributes) {
-        toRemove.clear();
-        for (int i = 0; i < noAttributes; i++) {
-            if (!toKeep.contains(i)) {
-                toRemove.add(i);
-            }
-        }
+	@Override
+	public void filter(Instance instance) {
+		if (toRemove.size() + toKeep.size() != instance.noAttributes())
+			buildRemove(instance.noAttributes());
+		instance.removeAttributes(toRemove);
+	}
 
-    }
+	private void buildRemove(int noAttributes) {
+		toRemove.clear();
+		for (int i = 0; i < noAttributes; i++) {
+			if (!toKeep.contains(i)) {
+				toRemove.add(i);
+			}
+		}
+
+	}
 }
