5eca886871d665e2309af7b580a5af15c288a21b
thomasabeel <thomasabeel@3162d2cb-3e1f-0410-96b0-914c58786302>
2009-10-12 18:22:33 +0000

    Allow reading data from Readers
    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@1203 3162d2cb-3e1f-0410-96b0-914c58786302

--- net/sf/javaml/tools/data/FileHandler.java
+++ net/sf/javaml/tools/data/FileHandler.java
@@ -6,18 +6,15 @@ package net.sf.javaml.tools.data;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.ZipInputStream;
 
 import net.sf.javaml.core.Dataset;
-import net.sf.javaml.core.DefaultDataset;
-import net.sf.javaml.core.DenseInstance;
 import net.sf.javaml.core.Instance;
 import net.sf.javaml.core.SparseInstance;
-import be.abeel.util.ColumnIterator;
 import be.abeel.util.GZIPPrintWriter;
-import be.abeel.util.LineIterator;
 
 /**
  * A class to load data sets from file and write them back.
@@ -30,17 +27,15 @@ import be.abeel.util.LineIterator;
  * Only class values are allowed to be non double. If a data set has no labels,
  * then all columns should be double values.
  * 
- * {@jmlSource}
  * 
  * @see net.sf.javaml.core.Dataset
  * @see net.sf.javaml.core.Instance
  * 
- * @version %SVN.REVISION%
  * 
  * @author Thomas Abeel
  * 
  */
-public class FileHandler {
+public class FileHandler extends StreamHandler{
 
     /**
      * Utility method to load from a file without class set.
@@ -79,78 +74,9 @@ public class FileHandler {
 
     }
 
-    private static Dataset loadSparse(java.io.InputStream in, int classIndex, String attSep, String indexSep) {
+    
 
-        ColumnIterator it = new ColumnIterator(in);
-        it.setDelimiter(attSep);
-        it.setSkipBlanks(true);
-        it.setSkipComments(true);
-        Dataset out = new DefaultDataset();
-        /* to keep track of the maximum number of attributes */
-        int maxAttributes = 0;
-        for (String[] arr : it) {
-            SparseInstance inst = new SparseInstance();
-
-            for (int i = 0; i < arr.length; i++) {
-                if (i == classIndex) {
-                    inst.setClassValue(arr[i]);
-                } else {
-                    String[] tmp = arr[i].split(indexSep);
-                    double val;
-                    try {
-                        val = Double.parseDouble(tmp[1]);
-                    } catch (NumberFormatException e) {
-                        val = Double.NaN;
-                    }
-                    inst.put(Integer.parseInt(tmp[0]), val);
-                }
-            }
-            if (inst.noAttributes() > maxAttributes)
-                maxAttributes = inst.noAttributes();
-            out.add(inst);
-
-        }
-        for (Instance inst : out) {
-            ((SparseInstance) inst).setNoAttributes(maxAttributes);
-        }
-        return out;
-    }
-
-    private static Dataset load(java.io.InputStream in, int classIndex, String separator) {
-
-        LineIterator it = new LineIterator(in);
-        it.setSkipBlanks(true);
-        it.setSkipComments(true);
-        Dataset out = new DefaultDataset();
-        for (String line : it) {
-            String[] arr = line.split(separator);
-            double[] values;
-            if (classIndex == -1)
-                values = new double[arr.length];
-            else
-                values = new double[arr.length - 1];
-            String classValue = null;
-            for (int i = 0; i < arr.length; i++) {
-                if (i == classIndex) {
-                    classValue = arr[i];
-                } else {
-                    double val;
-                    try {
-                        val = Double.parseDouble(arr[i]);
-                    } catch (NumberFormatException e) {
-                        val = Double.NaN;
-                    }
-                    if (classIndex != -1 && i > classIndex)
-                        values[i - 1] = val;
-                    else
-                        values[i] = val;
-                }
-            }
-            out.add(new DenseInstance(values, classValue));
-
-        }
-        return out;
-    }
+   
 
     /**
      * Load the data from a file.
@@ -176,10 +102,10 @@ public class FileHandler {
      */
     public static Dataset loadDataset(File f, int classIndex, String separator) throws IOException {
         if (f.getName().endsWith("gz"))
-            return load(new GZIPInputStream(new FileInputStream(f)), classIndex, separator);
+            return load(new InputStreamReader(new GZIPInputStream(new FileInputStream(f))), classIndex, separator);
         if (f.getName().endsWith("zip"))
-            return load(new ZipInputStream(new FileInputStream(f)), classIndex, separator);
-        return load(new FileInputStream(f), classIndex, separator);
+            return load(new InputStreamReader(new ZipInputStream(new FileInputStream(f))), classIndex, separator);
+        return load(new InputStreamReader(new FileInputStream(f)), classIndex, separator);
 
     }
 
@@ -191,10 +117,10 @@ public class FileHandler {
     public static Dataset loadSparseDataset(File f, int classIndex, String attributeSeparator, String indexSep)
             throws IOException {
         if (f.getName().endsWith("gz"))
-            return loadSparse(new GZIPInputStream(new FileInputStream(f)), classIndex, attributeSeparator, indexSep);
+            return loadSparse(new InputStreamReader(new GZIPInputStream(new FileInputStream(f))), classIndex, attributeSeparator, indexSep);
         if (f.getName().endsWith("zip"))
-            return loadSparse(new ZipInputStream(new FileInputStream(f)), classIndex, attributeSeparator, indexSep);
-        return loadSparse(new FileInputStream(f), classIndex, attributeSeparator, indexSep);
+            return loadSparse(new InputStreamReader(new ZipInputStream(new FileInputStream(f))), classIndex, attributeSeparator, indexSep);
+        return loadSparse(new InputStreamReader(new FileInputStream(f)), classIndex, attributeSeparator, indexSep);
     }
 
     /**
