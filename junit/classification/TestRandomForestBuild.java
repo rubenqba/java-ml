c9757c80406a6acd7cfd95ec0a5cbede46df1314
thomasabeel <thomasabeel@3162d2cb-3e1f-0410-96b0-914c58786302>
2012-10-11 01:10:48 +0000

    test for buildclassifier in RF, seems to work
    git-svn-id: https://svn.code.sf.net/p/java-ml/code/trunk@1275 3162d2cb-3e1f-0410-96b0-914c58786302

--- junit/classification/TestRandomForestBuild.java
+++ junit/classification/TestRandomForestBuild.java
@@ -7,6 +7,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Random;
 
+import junit.framework.Assert;
+
 import net.sf.javaml.classification.evaluation.EvaluateDataset;
 import net.sf.javaml.classification.tree.RandomForest;
 import net.sf.javaml.core.Dataset;
@@ -17,27 +19,44 @@ import org.junit.Test;
 import be.abeel.util.TimeInterval;
 
 public class TestRandomForestBuild {
-    @Test
-    public void testRF2Performance() {
-        long seed = System.currentTimeMillis();
-        try {
-            Dataset data = FileHandler.loadDataset(new File("devtools/data/iris.tsv"), 4, "\t");
-            System.out.println("Loader: " + data.classes());
-            RandomForest rf2 = new RandomForest(5, false, 2, new Random(seed));
-            System.out.println("Building 1");
-            rf2.buildClassifier(data.copy());
-            System.out.println("Building 2");
-            rf2.buildClassifier(data.copy());
-            System.out.println("Building 3");
-            rf2.buildClassifier(data.copy());
-            System.out.println(EvaluateDataset.testDataset(rf2, data));
+	@Test
+	public void testRFNPEConstructor() {
 
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        System.out.println(new TimeInterval(System.currentTimeMillis() - seed));
+		RandomForest rf=new RandomForest(4);
+		Dataset data;
+		try {
+			data = FileHandler.loadDataset(new File(
+					"devtools/data/iris.data"), 4, ",");
+			rf.buildClassifier(data);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			Assert.fail(e.getMessage());
+		}
+		
+	}
 
-    }
+	@Test
+	public void testRF2Performance() {
+		long seed = System.currentTimeMillis();
+		try {
+			Dataset data = FileHandler.loadDataset(new File("devtools/data/iris.tsv"), 4, "\t");
+			System.out.println("Loader: " + data.classes());
+			RandomForest rf2 = new RandomForest(5, false, 2, new Random(seed));
+			System.out.println("Building 1");
+			rf2.buildClassifier(data.copy());
+			System.out.println("Building 2");
+			rf2.buildClassifier(data.copy());
+			System.out.println("Building 3");
+			rf2.buildClassifier(data.copy());
+			System.out.println(EvaluateDataset.testDataset(rf2, data));
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(new TimeInterval(System.currentTimeMillis() - seed));
+
+	}
 
 }
